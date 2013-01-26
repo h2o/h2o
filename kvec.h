7aa0927bb0da67bd2317bb7dc7fe7c462598a2d5
Heng Li <lh3@me.com>
2013-01-26 18:09:06 -0500

    Two bugs reported by istreeter and wanghc78

--- kvec.h
+++ kvec.h
@@ -81,10 +81,10 @@ int main() {
 							(v).a = (type*)realloc((v).a, sizeof(type) * (v).m), 0)	\
 						   : 0), ((v).a + ((v).n++))
 
-#define kv_a(type, v, i) ((v).m <= (size_t)(i)?						\
+#define kv_a(type, v, i) (((v).m <= (size_t)(i)? \
 						  ((v).m = (v).n = (i) + 1, kv_roundup32((v).m), \
 						   (v).a = (type*)realloc((v).a, sizeof(type) * (v).m), 0) \
-						  : (v).n <= (size_t)(i)? (v).n = (i)			\
-						  : 0), (v).a[(i)]
+						  : (v).n <= (size_t)(i)? (v).n = (i) + 1 \
+						  : 0), (v).a[(i)])
 
 #endif
