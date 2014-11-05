06faa76f2eb70a88e5acc48cde437a469a3dfc17
Kazuho Oku <kazuhooku@gmail.com>
2014-11-06 06:27:39 +0900

    remove unnecessary #include <alloca.h>

--- examples/libh2o/simple.c
+++ examples/libh2o/simple.c
@@ -19,7 +19,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-#include <alloca.h>
 #include <errno.h>
 #include <limits.h>
 #include <netinet/in.h>
