5548f9f67953785f85ee36ccf36a407d6556397f
Kazuho Oku <kazuhooku@gmail.com>
2014-11-07 05:47:59 +0900

    [wip] update the http1client example to use the socket pool

--- examples/libh2o/http1client.c
+++ examples/libh2o/http1client.c
@@ -21,10 +21,55 @@
  */
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include "h2o/socketpool.h"
 #include "h2o/string_.h"
 #include "h2o/http1client.h"
 
-static int exit_status = -1;
+static h2o_timeout_t zero_timeout, io_timeout;
+static h2o_socketpool_t sockpool;
+static h2o_mempool_t pool;
+static const char *url;
+static int cnt_left = 2;
+
+static h2o_http1client_head_cb on_connect(h2o_http1client_t *client, const char *errstr, h2o_buf_t **reqbufs, size_t *reqbufcnt, int *method_is_head);
+static h2o_http1client_body_cb on_head(h2o_http1client_t *client, const char *errstr, int minor_version, int status, h2o_buf_t msg, struct phr_header *headers, size_t num_headers);
+
+static void start_request(h2o_http1client_ctx_t *ctx)
+{
+    char *scheme, *host, *path;
+    uint16_t port;
+    h2o_socket_t *sock;
+    h2o_buf_t *req;
+
+    /* clear memory pool */
+    h2o_mempool_clear(&pool);
+
+    /* parse URL */
+    if (h2o_parse_url(&pool, url, &scheme, &host, &port, &path) != 0) {
+        fprintf(stderr, "unrecognized type of URL: %s\n", url);
+        exit(1);
+    }
+    if (strcmp(scheme, "https") == 0) {
+        fprintf(stderr, "https is not (yet) supported\n");
+        exit(1);
+    }
+    /* build request */
+    req = h2o_mempool_alloc(&pool, sizeof(*req));
+    req->base = h2o_mempool_alloc(&pool, 1024);
+    req->len = snprintf(req->base, 1024, "GET %s HTTP/1.1\r\nhost: %s:%u\r\n\r\n", path, host, (unsigned)port);
+    assert(req->len < 1024);
+
+    /* initiate the request */
+    if ((sock = h2o_socketpool_acquire(&sockpool, ctx->loop)) != NULL) {
+        /* use the pool socket */
+        h2o_http1client_start(ctx, &pool, sock, req, 1, 0, on_head);
+    } else {
+        h2o_http1client_t *client = h2o_http1client_connect(ctx, &pool, host, port, on_connect);
+        assert(client != NULL);
+        client->data = req;
+    }
+}
 
 static int on_body(h2o_http1client_t *client, const char *errstr, h2o_buf_t *bufs, size_t bufcnt)
 {
@@ -32,26 +77,34 @@ static int on_body(h2o_http1client_t *client, const char *errstr, h2o_buf_t *buf
 
     if (errstr != NULL && errstr != h2o_http1client_error_is_eos) {
         fprintf(stderr, "%s\n", errstr);
-        exit_status = 1;
+        exit(1);
         return -1;
     }
 
     for (i = 0; i != bufcnt; ++i)
         fwrite(bufs[i].base, 1, bufs[i].len, stdout);
 
-    if (errstr == h2o_http1client_error_is_eos)
-        exit_status = 0;
+    if (errstr == h2o_http1client_error_is_eos) {
+        if (--cnt_left != 0) {
+            /* next attempt */
+            h2o_socket_t *sock;
+            if ((sock = h2o_http1client_detach_socket(client)) != NULL)
+                h2o_socketpool_register(&sockpool, sock, &io_timeout);
+            h2o_mempool_clear(&pool);
+            start_request(client->ctx);
+        }
+    }
 
     return 0;
 }
 
-static h2o_http1client_body_cb on_head(h2o_http1client_t *client, const char *errstr, int minor_version, int status, h2o_buf_t msg, struct phr_header *headers, size_t num_headers)
+h2o_http1client_body_cb on_head(h2o_http1client_t *client, const char *errstr, int minor_version, int status, h2o_buf_t msg, struct phr_header *headers, size_t num_headers)
 {
     size_t i;
 
     if (errstr != NULL && errstr != h2o_http1client_error_is_eos) {
         fprintf(stderr, "%s\n", errstr);
-        exit_status = 1;
+        exit(1);
         return NULL;
     }
 
@@ -61,18 +114,19 @@ static h2o_http1client_body_cb on_head(h2o_http1client_t *client, const char *er
     printf("\n");
 
     if (errstr == h2o_http1client_error_is_eos) {
-        exit_status = 0;
+        fprintf(stderr, "no body\n");
+        exit(1);
         return NULL;
     }
 
     return on_body;
 }
 
-static h2o_http1client_head_cb on_connect(h2o_http1client_t *client, const char *errstr, h2o_buf_t **reqbufs, size_t *reqbufcnt, int *method_is_head)
+h2o_http1client_head_cb on_connect(h2o_http1client_t *client, const char *errstr, h2o_buf_t **reqbufs, size_t *reqbufcnt, int *method_is_head)
 {
     if (errstr != NULL) {
         fprintf(stderr, "%s\n", errstr);
-        exit_status = 1;
+        exit(1);
         return NULL;
     }
 
@@ -85,34 +139,19 @@ static h2o_http1client_head_cb on_connect(h2o_http1client_t *client, const char 
 
 int main(int argc, char **argv)
 {
-    h2o_timeout_t zero_timeout, io_timeout;
     h2o_http1client_ctx_t ctx = {
         NULL,
         &zero_timeout,
         &io_timeout
     };
-    h2o_mempool_t pool;
-    char *scheme, *host, *path;
-    uint16_t port;
-    h2o_buf_t req;
-    h2o_http1client_t *client;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <url>\n", argv[0]);
         return 1;
     }
+    url = argv[1];
 
     h2o_mempool_init(&pool);
-
-    if (h2o_parse_url(&pool, argv[1], &scheme, &host, &port, &path) != 0) {
-        fprintf(stderr, "unrecognized type of URL: %s\n", argv[1]);
-        return 1;
-    }
-    if (strcmp(scheme, "https") == 0) {
-        fprintf(stderr, "https is not (yet) supported\n");
-        return 1;
-    }
-    req.len = asprintf((char**)&req.base, "GET %s HTTP/1.1\r\nhost: %s:%u\r\nconnection: close\r\n\r\n", path, host, (unsigned)port);
 
     /* setup context */
 #if H2O_USE_LIBUV
@@ -122,13 +161,12 @@ int main(int argc, char **argv)
 #endif
     h2o_timeout_init(ctx.loop, &zero_timeout, 0);
     h2o_timeout_init(ctx.loop, &io_timeout, 5000); /* 5 seconds */
+    h2o_socketpool_init(&sockpool, 0);
 
-    /* setup client */
-    client = h2o_http1client_connect(&ctx, &pool, host, port, on_connect);
-    assert(client != NULL);
-    client->data = &req;
+    /* setup the first request */
+    start_request(&ctx);
 
-    while (exit_status == -1) {
+    while (cnt_left != 0) {
 #if H2O_USE_LIBUV
         uv_run(ctx.loop, UV_RUN_DEFAULT);
 #else
@@ -136,5 +174,5 @@ int main(int argc, char **argv)
 #endif
     }
 
-    return exit_status;
+    return 0;
 }
