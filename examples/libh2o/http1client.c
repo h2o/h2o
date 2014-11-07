deacce86c426b281d954b5e899a77b101fa3b333
Kazuho Oku <kazuho@users.noreply.github.com>
2014-11-07 12:19:07 +0900

    define _GNU_SOURCE at cmake-level so as to suppress warnings caused by include order changes

--- examples/libh2o/http1client.c
+++ examples/libh2o/http1client.c
@@ -19,7 +19,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-#define _GNU_SOURCE
 #include <stdio.h>
 #include "h2o/string_.h"
 #include "h2o/http1client.h"
