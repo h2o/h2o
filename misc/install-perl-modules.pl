d48e12ef4a3ad0e02ff48ba2d1c2d019438c8113
Kazuho Oku <kazuhooku@gmail.com>
2014-11-11 05:59:18 +0900

    introduce a helper script for installing the necessary perl modules for testing

---
+++ misc/install-perl-modules.pl
@@ -0,0 +1,26 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+
+sub run_cmd {
+    my $cmd = shift;
+    print "$cmd\n";
+    system($cmd) == 0
+        or die "aborting..., command failed with $?";
+}
+
+sub install_module {
+    my $module = shift;
+    print "checking if $module is installed...\n";
+    if (system("perl -M$module -e '' > /dev/null 2>&1") != 0) {
+        run_cmd("cpanm --sudo --notest $module");
+    }
+}
+
+print "checking if cpanm is installed...\n";
+if (system("which cpanm > /dev/null 2>&1") != 0) {
+    run_cmd("curl -L http://cpanmin.us | perl - --sudo --notest App::cpanminus");
+}
+install_module($_)
+    for qw(Net::EmptyPort Scope::Guard Starlet);
