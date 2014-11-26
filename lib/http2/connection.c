c09be460c473d28b20ce19478977789eb16defdf
Kazuho Oku <kazuhooku@gmail.com>
2014-11-26 14:32:03 +0900

    max window size should be SETTINGS_HOST.initial_window_size

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -333,9 +333,10 @@ static void update_input_window(h2o_http2_conn_t *conn, uint32_t stream_id, h2o_
 {
     h2o_http2_window_consume_window(window, consumed);
     if (h2o_http2_window_get_window(window) * 2 < H2O_HTTP2_SETTINGS_HOST.initial_window_size) {
-        h2o_http2_encode_window_update_frame(&conn->_write.buf, stream_id, H2O_HTTP2_SETTINGS_HOST.initial_window_size);
+        int32_t delta = (int32_t)(H2O_HTTP2_SETTINGS_HOST.initial_window_size - h2o_http2_window_get_window(window));
+        h2o_http2_encode_window_update_frame(&conn->_write.buf, stream_id, delta);
         h2o_http2_conn_request_write(conn);
-        h2o_http2_window_update(window, H2O_HTTP2_SETTINGS_HOST.initial_window_size);
+        h2o_http2_window_update(window, delta);
     }
 }
 
