e0eb7a7d0eb1926b21ff2672f62701bb5cb50dbb
Kazuho Oku <kazuhooku@gmail.com>
2015-01-09 14:31:12 +0900

    implement h2o_add_header_token (for building Vary)

--- include/h2o.h
+++ include/h2o.h
@@ -497,6 +497,10 @@ void h2o_set_header(h2o_mem_pool_t *pool, h2o_headers_t *headers, const h2o_toke
  */
 void h2o_set_header_by_str(h2o_mem_pool_t *pool, h2o_headers_t *headers, const char *name, size_t name_len, int maybe_token, const char *value, size_t value_len, int overwrite_if_exists);
 /**
+ * adds a header token
+ */
+void h2o_add_header_token(h2o_mem_pool_t *pool, h2o_headers_t *headers, const h2o_token_t *token, const char *value, size_t value_len);
+/**
  * deletes a header from list
  */
 ssize_t h2o_delete_header(h2o_headers_t *headers, ssize_t cursor);
