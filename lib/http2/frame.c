6152efc61e48ff1bac291b25d36a905e1bb5e437
Kazuho Oku <kazuhooku@gmail.com>
2015-01-19 07:32:06 +0900

    report appropriate error for invalid PING frame

--- lib/http2/frame.c
+++ lib/http2/frame.c
@@ -201,10 +201,16 @@ int h2o_http2_decode_rst_stream_payload(h2o_http2_rst_stream_payload_t *payload,
     return 0;
 }
 
-int h2o_http2_decode_ping_payload(h2o_http2_ping_payload_t *payload, const h2o_http2_frame_t *frame)
+int h2o_http2_decode_ping_payload(h2o_http2_ping_payload_t *payload, const h2o_http2_frame_t *frame, const char **err_desc)
 {
-    if (frame->length != sizeof(payload->data))
-        return -1;
+    if (frame->stream_id != 0) {
+        *err_desc = "invalid PING frame";
+        return H2O_HTTP2_ERROR_PROTOCOL;
+    }
+    if (frame->length != sizeof(payload->data)) {
+        *err_desc = "invalid PING frame";
+        return H2O_HTTP2_ERROR_FRAME_SIZE;
+    }
 
     memcpy(payload->data, frame->payload, sizeof(payload->data));
     return 0;
