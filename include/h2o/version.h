aec3ace41bc7a919a99097b7d7b7842213a714e3
Kazuho Oku <kazuhooku@gmail.com>
2015-01-20 06:14:21 +0900

    releng

--- include/h2o/version.h
+++ include/h2o/version.h
@@ -22,10 +22,10 @@
 #ifndef h2o__version_h
 #define h2o__version_h
 
-#define H2O_VERSION "0.9.1-alpha1"
+#define H2O_VERSION "0.9.1"
 
 #define H2O_VERSION_MAJOR 0
 #define H2O_VERSION_MINOR 9
-#define H2O_VERSION_PATCH 0
+#define H2O_VERSION_PATCH 1
 
 #endif
