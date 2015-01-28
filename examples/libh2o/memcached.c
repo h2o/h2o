329428cb6e8a7c05ec02972f9a0f0d7ce78cd1c4
Kazuho Oku <kazuhooku@gmail.com>
2015-01-28 11:40:59 +0900

    add missing #includes found on linux

--- examples/libh2o/memcached.c
+++ examples/libh2o/memcached.c
@@ -19,6 +19,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
--- t/00unit/test.c
+++ t/00unit/test.c
@@ -19,6 +19,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
+#include <signal.h>
 #include "h2o/serverutil.h"
 #include "./test.h"
 
