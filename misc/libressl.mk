c34468c5c1ea437b75ac6579c5ca163133dbbb7a
Kazuho Oku <kazuhooku@gmail.com>
2015-03-17 04:38:24 +0900

    do not use`$(shell)`; not supported by NetBSD

--- misc/libressl.mk
+++ misc/libressl.mk
@@ -1,11 +1,11 @@
 SOURCE_DIR=.
 VERSION=2.1.4
 ARCHIVE=$(SOURCE_DIR)/libressl-$(VERSION).tar.gz
-DEST=$(shell pwd)/libressl-build
+DEST=libressl-build
 
 all: $(DEST)/lib/libssl.a
 
 $(DEST)/lib/libssl.a:
 	if [ ! -e "libressl-$(VERSION)" ] ; then tar xzf "$(ARCHIVE)" ; fi
-	if [ ! -e "libressl-$(VERSION)/Makefile" ] ; then (cd libressl-$(VERSION) && ./configure --prefix="$(DEST)" --disable-shared) ; fi
+	if [ ! -e "libressl-$(VERSION)/Makefile" ] ; then (P=`pwd`/$(DEST); cd libressl-$(VERSION) && ./configure --prefix="$$P" --disable-shared) ; fi
 	(cd libressl-$(VERSION) && make && make install)
