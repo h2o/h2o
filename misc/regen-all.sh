19bb6661c6686e1d1d03397606b78096d4286ec2
Kazuho Oku <kazuhooku@gmail.com>
2015-03-25 17:22:02 +0900

    build fat-packed version of `start_server` and install it under `share/h2o`

--- misc/regen-all.sh
+++ misc/regen-all.sh
@@ -3,3 +3,4 @@
 misc/tokens.pl || exit 1
 misc/picotemplate/picotemplate.pl --conf misc/picotemplate-conf.pl lib/handler/file/_templates.c.h || exit 1
 misc/clang-format-all.sh || exit 1
+(cd misc/p5-Server-Starter && fatpack-simple -o ../../share/h2o/start_server start_server) || exit 1
