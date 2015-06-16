c97e9de690a1855a69b5d8705c1182c33bf82484
Kazuho Oku <kazuhooku@gmail.com>
2015-06-16 16:25:22 +0900

    adjust fatpack script

--- misc/regen-all.sh
+++ misc/regen-all.sh
@@ -3,4 +3,4 @@
 misc/tokens.pl || exit 1
 misc/picotemplate/picotemplate.pl --conf misc/picotemplate-conf.pl lib/handler/file/_templates.c.h || exit 1
 misc/clang-format-all.sh || exit 1
-(cd misc/p5-Server-Starter && fatpack-simple -o ../../share/h2o/start_server start_server) || exit 1
+(cd misc/p5-Server-Starter && fatpack-simple --shebang '#! /bin/sh'$'\n''exec perl -x $0 "$@"'$'\n''#! perl' -o ../../share/h2o/start_server script/start_server) || exit 1
