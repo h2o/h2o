5728f6404cebcba9a51d46d126fbc7cf9c08fd07
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-07-18 20:07:27 +0900

    Apply clang-format

--- lib/handler/mruby/class/request.c
+++ lib/handler/mruby/class/request.c
@@ -31,7 +31,6 @@
 #include "mruby/class.h"
 #include <mruby/variable.h>
 
-
 static mrb_value h2o_mrb_req_init(mrb_state *mrb, mrb_value self)
 {
     return self;
@@ -50,17 +49,17 @@ static mrb_value h2o_mrb_req_log_error(mrb_state *mrb, mrb_value self)
 
 static mrb_value h2o_mrb_get_class_obj(mrb_state *mrb, mrb_value self, char *obj_id, char *class_name)
 {
-  mrb_value obj;
-  struct RClass *obj_class, *h2o_class;
+    mrb_value obj;
+    struct RClass *obj_class, *h2o_class;
 
-  obj = mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, obj_id));
-  if (mrb_nil_p(obj)) {
-    h2o_class = mrb_class_get(mrb, "H2O");
-    obj_class = (struct RClass*)mrb_class_ptr(mrb_const_get(mrb, mrb_obj_value(h2o_class), mrb_intern_cstr(mrb, class_name)));
-    obj = mrb_obj_new(mrb, obj_class, 0, NULL);
-    mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, obj_id), obj);
-  }
-  return obj;
+    obj = mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, obj_id));
+    if (mrb_nil_p(obj)) {
+        h2o_class = mrb_class_get(mrb, "H2O");
+        obj_class = (struct RClass *)mrb_class_ptr(mrb_const_get(mrb, mrb_obj_value(h2o_class), mrb_intern_cstr(mrb, class_name)));
+        obj = mrb_obj_new(mrb, obj_class, 0, NULL);
+        mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, obj_id), obj);
+    }
+    return obj;
 }
 
 static mrb_value h2o_mrb_headers_in_obj(mrb_state *mrb, mrb_value self)
@@ -73,68 +72,70 @@ static mrb_value h2o_mrb_headers_out_obj(mrb_state *mrb, mrb_value self)
     return h2o_mrb_get_class_obj(mrb, self, "headers_out_obj", "Headers_out");
 }
 
-static mrb_value h2o_mrb_get_request_headers_in(mrb_state *mrb, mrb_value self) 
+static mrb_value h2o_mrb_get_request_headers_in(mrb_state *mrb, mrb_value self)
 {
-  h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
-  mrb_value key;
-  ssize_t index;
-  h2o_header_t *h;
+    h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
+    mrb_value key;
+    ssize_t index;
+    h2o_header_t *h;
 
-  mrb_get_args(mrb, "o", &key);
-  key = mrb_funcall(mrb, key, "downcase", 0);
+    mrb_get_args(mrb, "o", &key);
+    key = mrb_funcall(mrb, key, "downcase", 0);
 
-  index = h2o_find_header_by_str(&mruby_ctx->req->headers, RSTRING_PTR(key), RSTRING_LEN(key), -1);
-  if (index == -1)
-    return mrb_nil_value();
+    index = h2o_find_header_by_str(&mruby_ctx->req->headers, RSTRING_PTR(key), RSTRING_LEN(key), -1);
+    if (index == -1)
+        return mrb_nil_value();
 
-  h = mruby_ctx->req->headers.entries + index;
+    h = mruby_ctx->req->headers.entries + index;
 
-  return mrb_str_new(mrb, h->value.base, h->value.len);
+    return mrb_str_new(mrb, h->value.base, h->value.len);
 }
 
-static mrb_value h2o_mrb_set_request_headers_in(mrb_state *mrb, mrb_value self) 
+static mrb_value h2o_mrb_set_request_headers_in(mrb_state *mrb, mrb_value self)
 {
-  h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
-  mrb_value key, val;
+    h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
+    mrb_value key, val;
 
-  mrb_get_args(mrb, "oo", &key, &val);
-  key = mrb_funcall(mrb, key, "downcase", 0);
+    mrb_get_args(mrb, "oo", &key, &val);
+    key = mrb_funcall(mrb, key, "downcase", 0);
 
-  h2o_set_header_by_str(&mruby_ctx->req->pool, &mruby_ctx->req->headers, RSTRING_PTR(key), RSTRING_LEN(key), 0, RSTRING_PTR(val), RSTRING_LEN(val), 1);
+    h2o_set_header_by_str(&mruby_ctx->req->pool, &mruby_ctx->req->headers, RSTRING_PTR(key), RSTRING_LEN(key), 0, RSTRING_PTR(val),
+                          RSTRING_LEN(val), 1);
 
-  return key;
+    return key;
 }
 
-static mrb_value h2o_mrb_get_request_headers_out(mrb_state *mrb, mrb_value self) 
+static mrb_value h2o_mrb_get_request_headers_out(mrb_state *mrb, mrb_value self)
 {
-  h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
-  mrb_value key;
-  ssize_t index;
-  h2o_header_t *h;
+    h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
+    mrb_value key;
+    ssize_t index;
+    h2o_header_t *h;
 
-  mrb_get_args(mrb, "o", &key);
-  key = mrb_funcall(mrb, key, "downcase", 0);
+    mrb_get_args(mrb, "o", &key);
+    key = mrb_funcall(mrb, key, "downcase", 0);
 
-  index = h2o_find_header_by_str(&mruby_ctx->req->res.headers, RSTRING_PTR(key), RSTRING_LEN(key), -1);
-  if (index == -1)
-    return mrb_nil_value();
+    index = h2o_find_header_by_str(&mruby_ctx->req->res.headers, RSTRING_PTR(key), RSTRING_LEN(key), -1);
+    if (index == -1)
+        return mrb_nil_value();
 
-  h = mruby_ctx->req->res.headers.entries + index;
+    h = mruby_ctx->req->res.headers.entries + index;
 
-  return mrb_str_new(mrb, h->value.base, h->value.len);
+    return mrb_str_new(mrb, h->value.base, h->value.len);
 }
 
-static mrb_value h2o_mrb_set_request_headers_out(mrb_state *mrb, mrb_value self) 
+static mrb_value h2o_mrb_set_request_headers_out(mrb_state *mrb, mrb_value self)
 {
-  h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
-  mrb_value key, val;
+    h2o_mruby_internal_context_t *mruby_ctx = (h2o_mruby_internal_context_t *)mrb->ud;
+    mrb_value key, val;
 
-  mrb_get_args(mrb, "oo", &key, &val);
-  key = mrb_funcall(mrb, key, "downcase", 0);
+    mrb_get_args(mrb, "oo", &key, &val);
+    key = mrb_funcall(mrb, key, "downcase", 0);
 
-  h2o_set_header_by_str(&mruby_ctx->req->pool, &mruby_ctx->req->res.headers, RSTRING_PTR(key), RSTRING_LEN(key), 0, RSTRING_PTR(val), RSTRING_LEN(val), 1);
+    h2o_set_header_by_str(&mruby_ctx->req->pool, &mruby_ctx->req->res.headers, RSTRING_PTR(key), RSTRING_LEN(key), 0,
+                          RSTRING_PTR(val), RSTRING_LEN(val), 1);
 
-  return key;
+    return key;
 }
 
 void h2o_mrb_request_class_init(mrb_state *mrb, struct RClass *class)
