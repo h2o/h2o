6b5062605f6e89941a77608a7be14e5f769fe1cc
Kazuho Oku <kazuhooku@gmail.com>
2015-07-19 11:47:07 +0900

    fix bug in hex encoder

--- lib/common/string.c
+++ lib/common/string.c
@@ -292,8 +292,8 @@ int h2o_hex_decode(void *_dst, const char *src, size_t src_len)
 
 void h2o_hex_encode(char *dst, const void *_src, size_t src_len)
 {
-    const unsigned char *src = _src;
-    for (; src_len != 0; --src_len) {
+    const unsigned char *src = _src, *src_end = src + src_len;
+    for (; src != src_end; ++src) {
         *dst++ = "0123456789abcdef"[*src >> 4];
         *dst++ = "0123456789abcdef"[*src & 0xf];
     }
