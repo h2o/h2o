6b15a8f0d1d2bf3bd47a84f0dcb3d8b3c7827cca
Kazuho Oku <kazuhooku@gmail.com>
2015-07-20 16:21:42 +0900

    update semver to 1.4.0-alpha1 (yes, we have new features)

--- include/h2o/version.h
+++ include/h2o/version.h
@@ -22,11 +22,11 @@
 #ifndef h2o__version_h
 #define h2o__version_h
 
-#define H2O_VERSION "1.3.2-alpha1"
+#define H2O_VERSION "1.4.0-alpha1"
 
 #define H2O_VERSION_MAJOR 1
-#define H2O_VERSION_MINOR 3
-#define H2O_VERSION_PATCH 2
+#define H2O_VERSION_MINOR 4
+#define H2O_VERSION_PATCH 0
 
 #define H2O_LIBRARY_VERSION_MAJOR 0
 #define H2O_LIBRARY_VERSION_MINOR 9
