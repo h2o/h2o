0fd84ef3198fdb2ba9cc58826d0cb27f1333710b
Kazuho Oku <kazuhooku@gmail.com>
2015-07-20 16:12:25 +0900

    update changes and copyrights

--- lib/common/multithread.c
+++ lib/common/multithread.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 DeNA Co., Ltd., Kazuho Oku
+ * Copyright (c) 2015 DeNA Co., Ltd., Kazuho Oku, Tatsuhiko Kubo
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
