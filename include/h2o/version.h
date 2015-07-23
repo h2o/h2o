2a3b3ec8617af9a4eaf8b3b2e7a8ba8c548593a2
Kazuho Oku <kazuhooku@gmail.com>
2015-07-23 10:31:15 +0900

    regenerate version.h

--- include/h2o/version.h
+++ include/h2o/version.h
@@ -22,14 +22,14 @@
 #ifndef h2o__version_h
 #define h2o__version_h
 
-#define H2O_VERSION "1.4.1"
+#define H2O_VERSION "1.4.2-alpha1"
 
 #define H2O_VERSION_MAJOR 1
 #define H2O_VERSION_MINOR 4
-#define H2O_VERSION_PATCH 1
+#define H2O_VERSION_PATCH 2
 
 #define H2O_LIBRARY_VERSION_MAJOR 0
 #define H2O_LIBRARY_VERSION_MINOR 9
-#define H2O_LIBRARY_VERSION_PATCH 13
+#define H2O_LIBRARY_VERSION_PATCH 14
 
 #endif
