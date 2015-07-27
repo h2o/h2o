b327f7079c88b2a18f3b54378adc545bc443e9ed
Justin Zhu <zlm2006@gmail.com>
2015-07-27 23:53:14 +0800

    modified as commented. added some guards on mimetype.

--- lib/handler/gzip.c
+++ lib/handler/gzip.c
@@ -4,77 +4,62 @@
 #include <zlib.h>
 #include "h2o.h"
 
-#define Z_GZIP_ENCODING 16
-#define MAX_BUF_SIZE 65000
+#define GZIP_ENCODING 16
+#define DEFAULT_WBITS 15
+#define DEFAULT_MEMLEVEL 8
+#define BUF_SIZE 8192
 
 typedef struct st_gzip_encoder_t {
     h2o_ostream_t super;
-    int wbits;
-    int memlevel;
     z_stream zstream;
-    h2o_mem_pool_t *mempool;
-    H2O_VECTOR(h2o_iovec_t) outbufs;
+    int started;
+    H2O_VECTOR(h2o_iovec_t) bufs;
 } gzip_encoder_t;
 
 static void *gzip_encoder_alloc(void *opaque, unsigned int items, unsigned int size)
 {
-    gzip_encoder_t *self = opaque;
-
-    /*
-     * I referenced from ngx_http_gzip_filter_module.c and write the code below (alloc len guard).
-     * Maybe this will help for memory alignment? IMHO The guard can be removed though.
-     */
-    unsigned int alloc = items * size;
-
-    if (alloc % 512 != 0 && alloc < 8192) {
-        alloc = 8192;
-    }
-
-    return h2o_mem_alloc_pool(self->mempool, alloc);
+    return h2o_mem_alloc(items * size);
 }
 
 static void gzip_encoder_free(void *opaque, void *address)
 {
-    /* we use mempool here, so no need of free */
+    free(address);
 }
 
 static void send_gzip(h2o_ostream_t *_self, h2o_req_t *req, h2o_iovec_t *inbufs, size_t inbufcnt, int is_final)
 {
     gzip_encoder_t *self = (void *)_self;
     
-    /*
-     * Well... The code below looks strange... I first tried to do gzip deflate in this way:
-     * Each time this function called, a buffer for output is allocated for all the input passed.
-     * If this call is not the final one, I just wait for the next call and do not pass the output
-     * to the next filter since I need to change Content-Length when the whole response has been
-     * deflated. So I don't need chunked filter anymore. Then the problem comes that I cannot get
-     * the next call since I don't pass the output to the next filter, the callback for sending
-     * next part of the response cannout be called...
-     *
-     * So I need to use chunked filter and the code becomes what it now looks like. Though chunked
-     * filter may cause kind of traffic inefficiency but it brings one benifit: lower pressure
-     * on memory since I do not need to store all the compress data in the memory. If this is okay
-     * I shall remove the "legacy" code.
-     *
-     * And a small question, how is chunked data processed on HTTP/2? split them into several DATA
-     * frames?
-     */
-    if (self->outbufs.size == self->outbufs.capacity) {
-        h2o_vector_reserve(&req->pool, (h2o_vector_t*)&self->outbufs, sizeof(h2o_iovec_t), self->outbufs.capacity + 1);
-    }
+    size_t in_total, i, outbuf_count = 1;
     
-    size_t out_size, i, outbuf_index;
-
-    /* calc gzip out buffer size for this time the same as in buffer total size */
-    out_size = 0;
+    /* calc in data total size */
+    in_total = 0;
     for (i = 0; i != inbufcnt; ++i)
-        out_size += inbufs[i].len;
+        in_total += inbufs[i].len;
 
-    outbuf_index = self->outbufs.size++;
-    self->outbufs.entries[outbuf_index].base = h2o_mem_alloc_pool(&req->pool, out_size);
-    /* set out buffer for output data from zlib */
-    self->zstream.next_out = (unsigned char *) self->outbufs.entries[outbuf_index].base;
-    self->zstream.avail_out = out_size;
+    if (self->started == 0) {
+        /* Initialization */
+        int ret = deflateInit2(&self->zstream, Z_DEFAULT_COMPRESSION, Z_DEFLATED,
+                               DEFAULT_WBITS + GZIP_ENCODING, DEFAULT_MEMLEVEL, Z_DEFAULT_STRATEGY);
+
+        assert(ret == Z_OK);
+
+        h2o_vector_reserve(&req->pool, (h2o_vector_t*)&self->bufs, sizeof(h2o_iovec_t),
+                           self->bufs.capacity + 1);
+        self->bufs.entries[0].base = h2o_mem_alloc_pool(&req->pool, BUF_SIZE);
+        self->bufs.entries[0].len = BUF_SIZE;
+        self->bufs.size = 1;
+
+        self->started = 1;
+    }
+
+    // At most (in_total / BUF_SIZE + 1) + 1 buffers for next level
+    h2o_iovec_t *outbufs = alloca(in_total / BUF_SIZE + 2);
+
+    /* set first out buffer for output data from zlib */
+    self->zstream.next_out = (unsigned char *) self->bufs.entries[0].base;
+    self->zstream.avail_out = self->bufs.entries[0].len;
+    outbufs[0] = self->bufs.entries[0];
 
     for (i = 0; i != inbufcnt; ++i) {
         int ret, flush;
@@ -87,24 +72,39 @@ static void send_gzip(h2o_ostream_t *_self, h2o_req_t *req, h2o_iovec_t *inbufs,
         } else {
             flush = Z_NO_FLUSH;
         }
-        ret = deflate(&self->zstream, flush);
-        if (ret != Z_OK && ret != Z_STREAM_END) {
-            /* Should log and a proper way */
-            assert(0);
+
+        while (1) {
+            ret = deflate(&self->zstream, flush);
+            assert(ret == Z_OK || ret == Z_STREAM_END);
+            if (ret == Z_OK && self->zstream.avail_out == 0) {
+                if (self->bufs.size == outbuf_count) {
+                    if (self->bufs.size == self->bufs.capacity) {
+                        h2o_vector_reserve(&req->pool, (h2o_vector_t*)&self->bufs, sizeof(h2o_iovec_t),
+                                           self->bufs.capacity + 1);
+                    }
+                    self->bufs.entries[self->bufs.size].base = h2o_mem_alloc_pool(&req->pool, BUF_SIZE);
+                    self->bufs.entries[self->bufs.size].len = BUF_SIZE;
+                    self->bufs.size++;
+                }
+                outbufs[outbuf_count - 1].len = BUF_SIZE;
+                outbufs[outbuf_count] = self->bufs.entries[outbuf_count];
+                self->zstream.next_out = (unsigned char *)outbufs[outbuf_count].base;
+                self->zstream.avail_out = outbufs[outbuf_count].len;
+                outbuf_count++;
+            } else
+                break;
         }
 
     }
-    self->outbufs.entries[outbuf_index].len = out_size - self->zstream.avail_out;
-    h2o_ostream_send_next(&self->super, req, &self->outbufs.entries[outbuf_index], 1, is_final);
+    outbufs[outbuf_count - 1].len = BUF_SIZE - self->zstream.avail_out;
+    h2o_ostream_send_next(&self->super, req, outbufs, outbuf_count, is_final);
 }
 
 static void on_setup_ostream(h2o_filter_t *self, h2o_req_t *req, h2o_ostream_t **slot)
 {
     gzip_encoder_t *encoder;
+    size_t header_index;
 
-    /* do nothing if content-length is unknown */
-    if (req->res.content_length == SIZE_MAX)
-        goto Next;
     /* RFC 2616 4.4 states that the following status codes (and response to a HEAD method) should not include message body */
     if ((100 <= req->res.status && req->res.status <= 199) || req->res.status == 204 || req->res.status == 304)
         goto Next;
@@ -115,63 +115,57 @@ static void on_setup_ostream(h2o_filter_t *self, h2o_req_t *req, h2o_ostream_t *
         req->http1_is_persistent = 0;
         goto Next;
     }
-    /* skip if transfer-encoding header is being set (for now) */
-    if (h2o_find_header(&req->res.headers, H2O_TOKEN_TRANSFER_ENCODING, -1) != -1)
+    /* skip if no accept-encoding is set */
+    if ((header_index = h2o_find_header(&req->headers, H2O_TOKEN_ACCEPT_ENCODING, -1)) == -1)
+        goto Next;
+    if (!h2o_contains_token(req->headers.entries[header_index].value.base,
+                            req->headers.entries[header_index].value.len, H2O_STRLIT("gzip"), ','))
         goto Next;
     /* skip if content-encoding header is being set */
     if (h2o_find_header(&req->res.headers, H2O_TOKEN_CONTENT_ENCODING, -1) != -1)
         goto Next;
+
     /* 
      * NOTE: I think there should be some test on MIME type, since we have no need to enable gzip on
-     *       those multimedia files or something like that.
+     *       those multimedia files or something like that. Maybe we can have a conf for this.
      */
+    header_index = h2o_find_header(&req->res.headers, H2O_TOKEN_CONTENT_TYPE, -1);
+    assert(header_index != -1);
+    if (h2o_contains_token(req->res.headers.entries[header_index].value.base,
+                           req->res.headers.entries[header_index].value.len, H2O_STRLIT("audio"), '/'))
+        goto Next;
+    if (h2o_contains_token(req->res.headers.entries[header_index].value.base,
+                           req->res.headers.entries[header_index].value.len, H2O_STRLIT("image"), '/'))
+        goto Next;
+    if (h2o_contains_token(req->res.headers.entries[header_index].value.base,
+                           req->res.headers.entries[header_index].value.len, H2O_STRLIT("video"), '/'))
+        goto Next;
+    if (h2o_contains_token(req->res.headers.entries[header_index].value.base,
+                           req->res.headers.entries[header_index].value.len, H2O_STRLIT("gzip"), '/'))
+        goto Next;
+    if (h2o_contains_token(req->res.headers.entries[header_index].value.base,
+                           req->res.headers.entries[header_index].value.len, H2O_STRLIT("zip"), '/'))
+        goto Next;
+    if (h2o_contains_token(req->res.headers.entries[header_index].value.base,
+                           req->res.headers.entries[header_index].value.len, H2O_STRLIT("zlib"), '/'))
+        goto Next;
 
     /* set content-encoding header */
     h2o_add_header(&req->pool, &req->res.headers, H2O_TOKEN_CONTENT_ENCODING, H2O_STRLIT("gzip"));
-    /* 
-     * For now, I need to change content_length to SIZE_MAX so I can enable chunked filter.
-     * I need chunked filter because I cannot know exactly how many times the "send_gzip()"
-     * function above will called at this point.
-     */
+
     req->res.content_length = SIZE_MAX;
 
     /* setup filter */
     encoder = (void *)h2o_add_ostream(req, sizeof(gzip_encoder_t), slot);
     encoder->super.do_send = send_gzip;
     slot = &encoder->super.next;
-    
-    /*
-     * TBH, I don't know exactly what windowbits & memlevel do in zlib deflate/inflate procedure.
-     * I just made a reference from ngx_http_gzip_filter_module.c
-     */
-    int wbits = 15, memlevel = 8;
-    
-    while (memlevel > 0 && req->res.content_length < ((1 << (wbits - 1)) - 262)) {
-        wbits--;
-        memlevel--;
-    }
-    
-    if (memlevel < 1) {
-        memlevel = 1;
-    }
-    
-    encoder->wbits = wbits | Z_GZIP_ENCODING;
-    encoder->memlevel = memlevel;
-    encoder->outbufs.capacity = 0;
-    encoder->outbufs.size = 0;
+
+    encoder->bufs.capacity = 0;
+    encoder->bufs.size = 0;
     encoder->zstream.zalloc = gzip_encoder_alloc;
     encoder->zstream.zfree = gzip_encoder_free;
     encoder->zstream.opaque = encoder;
-    encoder->mempool = &req->pool;
-    
-    /* I think there could be some configuration options on the compression strategy? */
-    int ret = deflateInit2(&encoder->zstream, Z_DEFAULT_COMPRESSION, Z_DEFLATED,
-                           encoder->wbits, encoder->memlevel, Z_DEFAULT_STRATEGY);
-    
-    if (ret != Z_OK) {
-        /* Should log and handle in a proper way */
-        assert(0);
-    }
+    encoder->started = 0;
 
 Next:
     h2o_setup_next_ostream(self, req, slot);
