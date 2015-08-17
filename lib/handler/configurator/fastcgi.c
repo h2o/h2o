78bb6fb0f5c9424d1173e5acfde694d8a3bd98dd
Kazuho Oku <kazuhooku@gmail.com>
2015-08-17 10:15:31 +0900

    change ownership of domain socket for fastcgi (fixes #443)
    Conflicts:
    	lib/handler/configurator/fastcgi.c

--- lib/handler/configurator/fastcgi.c
+++ lib/handler/configurator/fastcgi.c
@@ -25,6 +25,7 @@
 #include <inttypes.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <pwd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/un.h>
@@ -146,8 +147,8 @@ static int on_config_connect(h2o_configurator_command_t *cmd, h2o_configurator_c
     return 0;
 }
 
-static int create_spawnproc(h2o_configurator_command_t *cmd, yoml_t *node, const char *dirname, const char **argv,
-                            struct sockaddr_un *sa)
+static int create_spawnproc(h2o_configurator_command_t *cmd, yoml_t *node, const char *dirname, char *const *argv,
+                            struct sockaddr_un *sa, struct passwd *pw)
 {
     int listen_fd, pipe_fds[2] = {-1, -1};
 
@@ -167,6 +168,11 @@ static int create_spawnproc(h2o_configurator_command_t *cmd, yoml_t *node, const
     }
     if (listen(listen_fd, SOMAXCONN) != 0) {
         h2o_configurator_errprintf(cmd, node, "listen(2) failed: %s", strerror(errno));
+        goto Error;
+    }
+    /* change ownership of socket */
+    if (pw != NULL && chown(sa->sun_path, pw->pw_uid, pw->pw_gid) != 0) {
+        h2o_configurator_errprintf(cmd, node, "chown(2) failed to change ownership of socket:%s:%s", sa->sun_path, strerror(errno));
         goto Error;
     }
 
@@ -223,6 +229,8 @@ static int on_config_spawn(h2o_configurator_command_t *cmd, h2o_configurator_con
     struct sockaddr_un sa = {};
     h2o_fastcgi_config_vars_t config_vars;
     int ret = -1;
+    struct passwd spawn_pwbuf, *spawn_pw;
+    char spawn_buf[65536];
 
     switch (node->type) {
     case YOML_TYPE_SCALAR:
@@ -240,6 +248,7 @@ static int on_config_spawn(h2o_configurator_command_t *cmd, h2o_configurator_con
             return -1;
         }
         spawn_cmd = t->data.scalar;
+        spawn_user = ctx->globalconf->user;
         if ((t = yoml_get(node, "user")) != NULL) {
             if (t->type != YOML_TYPE_SCALAR) {
                 h2o_configurator_errprintf(cmd, node, "attribute `user` must be scalar");
@@ -253,15 +262,30 @@ static int on_config_spawn(h2o_configurator_command_t *cmd, h2o_configurator_con
         return -1;
     }
 
+    /* obtain uid & gid of spawn_user */
+    if (spawn_user != NULL) {
+        /* change ownership of temporary directory */
+        if (getpwnam_r(spawn_user, &spawn_pwbuf, spawn_buf, sizeof(spawn_buf), &spawn_pw) != 0) {
+            h2o_configurator_errprintf(cmd, node, "getpwnam_r(3) failed to get password file entry");
+            goto Exit;
+        }
+        if (spawn_pw == NULL) {
+            h2o_configurator_errprintf(cmd, node, "unknown user:%s", spawn_user);
+            goto Exit;
+        }
+    } else {
+        spawn_pw = NULL;
+    }
+
     { /* build args */
         size_t i = 0;
         argv[i++] = h2o_configurator_get_cmd_path("share/h2o/kill-on-close");
         argv[i++] = "--rm";
         argv[i++] = dirname;
         argv[i++] = "--";
-        if (spawn_user != NULL) {
+        if (spawn_pw != NULL) {
             argv[i++] = h2o_configurator_get_cmd_path("share/h2o/setuidgid");
-            argv[i++] = spawn_user;
+            argv[i++] = spawn_pw->pw_name;
         }
         argv[i++] = "/bin/sh";
         argv[i++] = "-c";
@@ -283,8 +307,14 @@ static int on_config_spawn(h2o_configurator_command_t *cmd, h2o_configurator_con
             dirname[0] = '\0';
             goto Exit;
         }
+        /* change ownership of temporary directory */
+        if (spawn_pw != NULL && chown(dirname, spawn_pw->pw_uid, spawn_pw->pw_gid) != 0) {
+            h2o_configurator_errprintf(cmd, node, "chown(2) failed to change ownership of temporary directory:%s:%s", dirname,
+                                       strerror(errno));
+            goto Exit;
+        }
         /* launch spawnfcgi command */
-        if ((spawner_fd = create_spawnproc(cmd, node, dirname, argv, &sa)) == -1) {
+        if ((spawner_fd = create_spawnproc(cmd, node, dirname, argv, &sa, spawn_pw)) == -1) {
             goto Exit;
         }
     }
---
+++ t/90root-fastcgi-php.t
@@ -0,0 +1,76 @@
+# this test checks the behavior of `file.custom-handler` and `fastcgi.spawn`
+use strict;
+use warnings;
+use Digest::MD5 qw(md5_hex);
+use File::Temp qw(tempdir);
+use Net::EmptyPort qw(check_port empty_port);
+use Test::More;
+use t::Util;
+
+plan skip_all => 'test requires root privileges'
+    unless $< == 0;
+plan skip_all => 'user: nobody does not exist'
+    unless getpwnam 'nobody';
+
+plan skip_all => 'curl not found'
+    unless prog_exists('curl');
+plan skip_all => 'php-cgi not found'
+    unless prog_exists('php-cgi');
+
+sub check_resp {
+    my $port = shift;
+    my $resp = `curl --silent http://127.0.0.1:$port/index.txt`;
+    is $resp, "hello\n", 'ordinary file';
+    $resp = `curl --silent http://127.0.0.1:$port/hello.php`;
+    is $resp, 'hello world', 'php';
+}
+
+subtest 'user-in-global-conf' => sub {
+    my $server = spawn_h2o(<< "EOT");
+user: nobody
+file.custom-handler:
+  extension: .php
+  fastcgi.spawn: "exec php-cgi"
+hosts:
+  default:
+    paths:
+      "/":
+        file.dir: @{[ DOC_ROOT ]}
+EOT
+    check_resp($server->{port});
+};
+
+subtest 'user-in-fastcgi.spawn' => sub {
+    my $server = spawn_h2o(<< "EOT");
+user: nobody
+file.custom-handler:
+  extension: .php
+  fastcgi.spawn:
+    command: "exec php-cgi"
+    user:    nobody
+hosts:
+  default:
+    paths:
+      "/":
+        file.dir: @{[ DOC_ROOT ]}
+EOT
+    check_resp($server->{port});
+};
+
+subtest 'user-not-in-map-style-fastcgi.spawn' => sub {
+    my $server = spawn_h2o(<< "EOT");
+user: nobody
+file.custom-handler:
+  extension: .php
+  fastcgi.spawn:
+    command: "exec php-cgi"
+hosts:
+  default:
+    paths:
+      "/":
+        file.dir: @{[ DOC_ROOT ]}
+EOT
+    check_resp($server->{port});
+};
+
+done_testing();
