0e42aee25530e00f5d9068ccd49e014dbd336edd
Kazuho Oku <kazuhooku@gmail.com>
2015-08-27 15:01:27 +0900

    do not send `SETTINGS_ENABLE_PUSH` to client

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -53,12 +53,10 @@ const h2o_http2_settings_t H2O_HTTP2_SETTINGS_HOST = {
     16384   /* max_frame_size */
 };
 
-static const h2o_iovec_t SETTINGS_HOST_BIN = {H2O_STRLIT("\x00\x00\x12"     /* frame size */
+static const h2o_iovec_t SETTINGS_HOST_BIN = {H2O_STRLIT("\x00\x00\x0c"     /* frame size */
                                                          "\x04"             /* settings frame */
                                                          "\x00"             /* no flags */
                                                          "\x00\x00\x00\x00" /* stream id */
-                                                         "\x00\x02"
-                                                         "\x00\x00\x00\x00" /* enable_push = 0 */
                                                          "\x00\x03"
                                                          "\x00\x00\x00\x64" /* max_concurrent_streams = 100 */
                                                          "\x00\x04"
