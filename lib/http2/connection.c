32aa44425c62210ca295e4cba0d7690bc79adc68
Kazuho Oku <kazuhooku@gmail.com>
2015-09-18 12:22:51 +0900

    call `write(2)` for every 16k

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -934,6 +934,7 @@ static int emit_writereq_of_openref(h2o_http2_scheduler_openref_t *ref, int *sti
 {
     h2o_http2_conn_t *conn = cb_arg;
     h2o_http2_stream_t *stream = H2O_STRUCT_FROM_MEMBER(h2o_http2_stream_t, _refs.scheduler, ref);
+    ssize_t conn_window_size = h2o_http2_conn_get_buffer_window(conn);
 
     assert(h2o_http2_stream_has_pending_data(stream) || stream->state == H2O_HTTP2_STREAM_STATE_END_STREAM);
 
@@ -950,7 +951,8 @@ static int emit_writereq_of_openref(h2o_http2_scheduler_openref_t *ref, int *sti
         h2o_linklist_insert(&conn->_write.streams_to_proceed, &stream->_refs.link);
     }
 
-    return h2o_http2_conn_get_buffer_window(conn) > 0 ? 0 : -1;
+    /* request bail out if we have written any data */
+    return conn_window_size != h2o_http2_conn_get_buffer_window(conn);
 }
 
 int do_emit_writereq(h2o_http2_conn_t *conn)
