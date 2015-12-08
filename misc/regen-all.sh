da8564d49f15ba0327aa3dd2c6169096cd1ca062
Kazuho Oku <kazuhooku@gmail.com>
2015-12-09 06:23:09 +0900

    convert regen.sh to Makefile

--- misc/regen-all.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-misc/tokens.pl || exit 1
-misc/picotemplate/picotemplate.pl --conf misc/picotemplate-conf.pl lib/handler/file/_templates.c.h || exit 1
-misc/clang-format-all.sh || exit 1
-(cd misc/p5-Server-Starter && fatpack-simple --shebang '#! /bin/sh'$'\n''exec perl -x $0 "$@"'$'\n''#! perl' -o ../../share/h2o/start_server script/start_server) || exit 1
-(cd misc/p5-net-fastcgi && fatpack-simple --shebang '#! /bin/sh'$'\n''exec perl -x $0 "$@"'$'\n''#! perl' -o ../../share/h2o/fastcgi-cgi ../fastcgi-cgi.pl) || exit 1
---
+++ misc/regen.mk
@@ -0,0 +1,29 @@
+define FATPACK_SHEBANG
+#! /bin/sh
+exec perl -x $$0 "$$@"
+#! perl
+endef
+export FATPACK_SHEBANG
+
+all: tokens lib/handler/file/templates.c.h clang-format-all share/h2o/start_server share/h2o/fastcgi-cgi
+
+tokens:
+	misc/tokens.pl
+
+lib/handler/file/templates.c.h:
+	misc/picotemplate/picotemplate.pl --conf misc/picotemplate-conf.pl lib/handler/file/_templates.c.h || exit 1
+
+clang-format-all:
+	misc/clang-format-all.sh
+
+share/h2o/start_server: FORCE
+	cd misc/p5-Server-Starter; \
+	fatpack-simple --shebang "$$FATPACK_SHEBANG" -o ../../$@ script/start_server
+
+share/h2o/fastcgi-cgi: FORCE
+	cd misc/p5-net-fastcgi; \
+	fatpack-simple --shebang "$$FATPACK_SHEBANG" -o ../../$@ ../fastcgi-cgi.pl
+
+FORCE:
+
+.PHONY: tokens clang-format-all FORCE
