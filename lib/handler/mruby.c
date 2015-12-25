3be2a08270e37f5d7b2961e627188159c33a062b
Kazuho Oku <kazuhooku@gmail.com>
2015-12-25 16:08:27 +0900

    update copyright

--- lib/handler/mruby.c
+++ lib/handler/mruby.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2014,2015 DeNA Co., Ltd., Kazuho Oku, Ryosuke Matsumoto
+ * Copyright (c) 2014,2015 DeNA Co., Ltd., Kazuho Oku, Ryosuke Matsumoto,
+                           Masayoshi Takahashi
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
