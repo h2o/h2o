7ecd510ca640e6671af0e4bfc6a7842d7b815095
Kazuho Oku <kazuhooku@gmail.com>
2016-02-02 13:13:49 +0900

    rebuild libressl.a if the tarball gets replaced

--- misc/libressl.mk
+++ misc/libressl.mk
@@ -6,7 +6,7 @@ UNAME=$(shell uname -s)
 
 all: $(DEST)/lib/libssl.a
 
-$(DEST)/lib/libssl.a:
+$(DEST)/lib/libssl.a: $(ARCHIVE)
 	if [ ! -e "libressl-$(VERSION)" ] ; then tar xzf "$(ARCHIVE)" ; fi
 	if [ ! -e "libressl-$(VERSION)/Makefile" ] ; then (P=`pwd`/$(DEST); cd libressl-$(VERSION) && ./configure --prefix="$$P" --libdir="$$P/lib" --disable-shared `test "$(UNAME)" = "Darwin" && echo '--disable-asm'`) ; fi
 	(cd libressl-$(VERSION) && make && make install)
