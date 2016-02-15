712419875e69a964ce321a041c6f5c0dec8c87c9
Kazuho Oku <kazuhooku@gmail.com>
2016-02-16 04:37:25 +0900

    incr version number for dev

--- include/h2o/version.h
+++ include/h2o/version.h
@@ -22,14 +22,14 @@
 #ifndef h2o__version_h
 #define h2o__version_h
 
-#define H2O_VERSION "1.7.0"
+#define H2O_VERSION "1.8.0-alpha1"
 
 #define H2O_VERSION_MAJOR 1
-#define H2O_VERSION_MINOR 7
+#define H2O_VERSION_MINOR 8
 #define H2O_VERSION_PATCH 0
 
 #define H2O_LIBRARY_VERSION_MAJOR 0
 #define H2O_LIBRARY_VERSION_MINOR 9
-#define H2O_LIBRARY_VERSION_PATCH 21
+#define H2O_LIBRARY_VERSION_PATCH 22
 
 #endif
