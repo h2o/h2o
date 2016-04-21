d33f2690426f7d8771cca252dfbf1f6c316502be
Kazuho Oku <kazuhooku@gmail.com>
2016-04-21 10:22:18 +0900

    support partial-archive dump

--- misc/dump-github-repository.pl
+++ misc/dump-github-repository.pl
@@ -5,19 +5,33 @@ use warnings;
 use Errno ();
 use File::Basename qw(basename);
 
-die "Usage: $0 <https://github.com/user/repo> <commit> <dest-dir>\n"
-    unless @ARGV == 3;
+die "Usage: $0 <https://github.com/user/repo> <commit> <dest-dir> [<path>]\n"
+    if @ARGV < 3;
 
-my ($repo, $commit, $dest) = @ARGV;
+my ($repo, $commit, $dest, $path) = @ARGV;
+my $strip_components = 1;
+my ($rm_path, $tar_path);
 
-run("rm -rf $dest");
+if (defined $path) {
+    $path =~ s|/*$||;
+    $strip_components += scalar(split "/", $path) - 1;
+    $rm_path = "$dest/" . basename $path;
+    $tar_path = "*/$path";
+} else {
+    $path = "";
+    $rm_path = "$dest";
+    $tar_path = "";
+}
+
+run("rm -rf $rm_path");
+
 mkdir("$dest")
     or $! == Errno::EEXIST or die "failed to (re)create directory:$dest:$!";
-run("curl --silent --show-error --location $repo/archive/$commit.tar.gz | (cd $dest && tar x --strip-components 1 -zf -)") == 0
+run("curl --silent --show-error --location $repo/archive/$commit.tar.gz | (cd $dest && tar x --strip-components $strip_components -zf - $tar_path)") == 0
     or die "failed to extract $repo/archive/$commit.tar.gz to $dest";
-run("git add -f `find $dest -type f`") == 0
+run("git add -f `find $rm_path -type f`") == 0
     or die "failed to add files under $dest";
-run("git commit -m 'extract $repo @ $commit at $dest' $dest") == 0
+run("git commit -m 'extract $repo @ $commit @{[defined $path ? qq{($path)} : '']} at $dest' $dest") == 0
     or die "failed to commit";
 
 sub run {
