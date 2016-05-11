25d5a6ff5169d6762748f8706c5ce77bb8ac691f
Frederik Deweerdt <fdeweerdt@fastly.com>
2016-05-11 10:56:28 -0700

    Fix `h2o_cache_calchash` to take into account the whole key value when computing the hash value.

--- lib/common/cache.c
+++ lib/common/cache.c
@@ -106,7 +106,7 @@ h2o_cache_hashcode_t h2o_cache_calchash(const char *s, size_t l)
 {
     h2o_cache_hashcode_t h = 0;
     for (; l != 0; --l)
-        h = (h << 5) - h + *(unsigned char *)s;
+        h = (h << 5) - h + ((unsigned char *)s)[l - 1];
     return h;
 }
 
