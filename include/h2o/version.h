1ea1fb6bfea9d1e17324faf583d575ddab6dfa7f
Kazuho Oku <kazuhooku@gmail.com>
2016-05-26 11:39:16 +0900

    update version to 1.7.3/0.9.24

--- include/h2o/version.h
+++ include/h2o/version.h
@@ -22,14 +22,14 @@
 #ifndef h2o__version_h
 #define h2o__version_h
 
-#define H2O_VERSION "1.7.2"
+#define H2O_VERSION "1.7.3"
 
 #define H2O_VERSION_MAJOR 1
 #define H2O_VERSION_MINOR 7
-#define H2O_VERSION_PATCH 2
+#define H2O_VERSION_PATCH 3
 
 #define H2O_LIBRARY_VERSION_MAJOR 0
 #define H2O_LIBRARY_VERSION_MINOR 9
-#define H2O_LIBRARY_VERSION_PATCH 23
+#define H2O_LIBRARY_VERSION_PATCH 24
 
 #endif
