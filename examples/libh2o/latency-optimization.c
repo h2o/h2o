0a6fbe742968ca6e02bfe8aa9e3d8840c84c9063
Kazuho Oku <kazuhooku@gmail.com>
2016-06-14 11:19:17 +0900

    wip

--- examples/libh2o/latency-optimization.c
+++ examples/libh2o/latency-optimization.c
@@ -256,8 +256,8 @@ int main(int argc, char **argv)
         OpenSSL_add_all_algorithms();
         if (mode_server) {
             ssl_ctx = SSL_CTX_new(TLSv1_server_method());
-            SSL_CTX_use_certificate_file(ssl_ctx, H2O_TO_STR(H2O_ROOT) "/examples/h2o/server.crt", SSL_FILETYPE_PEM);
-            SSL_CTX_use_PrivateKey_file(ssl_ctx, H2O_TO_STR(H2O_ROOT) "/examples/h2o/server.key", SSL_FILETYPE_PEM);
+            SSL_CTX_use_certificate_file(ssl_ctx, "examples/h2o/server.crt", SSL_FILETYPE_PEM);
+            SSL_CTX_use_PrivateKey_file(ssl_ctx, "examples/h2o/server.key", SSL_FILETYPE_PEM);
         } else {
             ssl_ctx = SSL_CTX_new(TLSv1_client_method());
         }
