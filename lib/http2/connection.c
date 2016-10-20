c836b793d3506b97ea2373dd68ab6452479eb0e3
Frederik Deweerdt <fdeweerdt@fastly.com>
2016-10-20 09:48:27 -0700

    Close the connection at the end of the graceful shutdown
    On graceful shutdown, after the second GOAWAY message has been written,
    it should be possible for use to safely close the connection, without
    necessarily waiting for all streams to be processed.

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -88,8 +88,10 @@ static void graceful_shutdown_resend_goaway(h2o_timeout_entry_t *entry)
 
     for (node = ctx->http2._conns.next; node != &ctx->http2._conns; node = node->next) {
         h2o_http2_conn_t *conn = H2O_STRUCT_FROM_MEMBER(h2o_http2_conn_t, _conns, node);
-        if (conn->state < H2O_HTTP2_CONN_STATE_HALF_CLOSED)
+        if (conn->state < H2O_HTTP2_CONN_STATE_HALF_CLOSED) {
             enqueue_goaway(conn, H2O_HTTP2_ERROR_NONE, (h2o_iovec_t){NULL});
+            close_connection(conn);
+        }
     }
 }
 
