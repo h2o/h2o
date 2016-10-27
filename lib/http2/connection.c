00126d7bd61cf9a39b5cb9217bb5036d6b4a29d8
Frederik Deweerdt <fdeweerdt@fastly.com>
2016-10-27 08:23:17 -0700

    Add a second timeout callback, in which we close the connection.
    This gives extra time to clients after we've sent the second GOAWAY
    frame.

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -81,17 +81,39 @@ static void enqueue_goaway(h2o_http2_conn_t *conn, int errnum, h2o_iovec_t addit
     }
 }
 
+static void graceful_shutdown_close_stragglers(h2o_timeout_entry_t *entry)
+{
+    h2o_context_t *ctx = H2O_STRUCT_FROM_MEMBER(h2o_context_t, http2._graceful_shutdown_timeout, entry);
+    h2o_linklist_t *node, *next;
+
+    /* We've sent two GOAWAY frames, close the remaining connections */
+    for (node = ctx->http2._conns.next; node != &ctx->http2._conns; node = next) {
+        h2o_http2_conn_t *conn = H2O_STRUCT_FROM_MEMBER(h2o_http2_conn_t, _conns, node);
+        next = node->next;
+        if (conn->state < H2O_HTTP2_CONN_STATE_IS_CLOSING) {
+            close_connection(conn);
+        }
+    }
+}
+
 static void graceful_shutdown_resend_goaway(h2o_timeout_entry_t *entry)
 {
     h2o_context_t *ctx = H2O_STRUCT_FROM_MEMBER(h2o_context_t, http2._graceful_shutdown_timeout, entry);
     h2o_linklist_t *node;
+    int do_close_stragglers = 0;
 
     for (node = ctx->http2._conns.next; node != &ctx->http2._conns; node = node->next) {
         h2o_http2_conn_t *conn = H2O_STRUCT_FROM_MEMBER(h2o_http2_conn_t, _conns, node);
         if (conn->state < H2O_HTTP2_CONN_STATE_HALF_CLOSED) {
             enqueue_goaway(conn, H2O_HTTP2_ERROR_NONE, (h2o_iovec_t){NULL});
-            close_connection(conn);
+            do_close_stragglers = 1;
         }
+    }
+
+    /* after waiting a second, we still had active connections, wait one more second */
+    if (do_close_stragglers) {
+        ctx->http2._graceful_shutdown_timeout.cb = graceful_shutdown_close_stragglers;
+        h2o_timeout_link(ctx->loop, &ctx->one_sec_timeout, &ctx->http2._graceful_shutdown_timeout);
     }
 }
 
