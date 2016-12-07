d900f896fff0986aaf8a708867e98ee65f81a2dd
Frederik Deweerdt <fdeweerdt@fastly.com>
2016-12-07 10:48:02 -0800

    add missing unlink

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -1329,6 +1329,7 @@ int h2o_http2_handle_upgrade(h2o_req_t *req, struct timeval connected_at)
 
     return 0;
 Error:
+    h2o_linklist_unlink(&http2conn->_conns);
     free(http2conn);
     return -1;
 }
