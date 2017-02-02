62c011045059e312324a33bd4daed409eb34f742
Ichito Nagata <i.nagata110@gmail.com>
2017-02-02 16:47:46 +0900

    fix license texts

--- misc/regen.mk
+++ misc/regen.mk
@@ -11,7 +11,7 @@ tokens:
 	misc/tokens.pl
 
 lib/handler/mruby/embedded.c.h: misc/embed_mruby_code.pl \
-								lib/handler/mruby/embedded/core.rb \
+                                lib/handler/mruby/embedded/core.rb \
                                 lib/handler/mruby/embedded/http_request.rb \
                                 lib/handler/mruby/embedded/chunked.rb
 	misc/embed_mruby_code.pl $^ > $@
