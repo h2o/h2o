35fefca5cf61b56998b2d84e77aed94605e8f10e
Ichito Nagata <i.nagata110@gmail.com>
2017-03-23 11:25:10 +0900

    use patched Test-TCP module to avoid "Address already in use" problem

--- misc/install-perl-module.pl
+++ misc/install-perl-module.pl
@@ -2,6 +2,12 @@
 
 use strict;
 use warnings;
+use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
+
+my $nocheck;
+GetOptions(
+    nocheck => \$nocheck,
+) or exit 1;
 
 sub run_cmd {
     my $cmd = shift;
@@ -12,10 +18,13 @@ sub run_cmd {
 
 sub install_module {
     my $module = shift;
-    print "checking if $module is installed...\n";
-    if (system("perl -M$module -e '' > /dev/null 2>&1") != 0) {
-        run_cmd("cpanm --sudo --notest $module");
+    unless ($nocheck) {
+        print "checking if $module is installed...\n";
+        if (system("perl -M$module -e '' > /dev/null 2>&1") == 0) {
+            return;
+        }
     }
+    run_cmd("cpanm --sudo --notest $module");
 }
 
 print "checking if cpanm is installed...\n";
