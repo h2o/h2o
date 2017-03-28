05696747e93c02580bd0bd67c3f722be387eaf76
Frederik Deweerdt <fdeweerdt@fastly.com>
2017-03-28 10:46:41 -0700

    Timeout even if there a request body being posted, remove the assert, since there might be pending requests when the timeout is initialized. Add test coverage

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -134,9 +134,8 @@ static void update_idle_timeout(h2o_http2_conn_t *conn)
 {
     h2o_timeout_unlink(&conn->_timeout_entry);
 
-    /* no active streams + no request body upload in progress */
-    if (conn->num_streams.pull.half_closed + conn->num_streams.push.half_closed == 0 && !conn->_request_body_in_progress) {
-        assert(h2o_linklist_is_empty(&conn->_pending_reqs));
+    /* no active streams */
+    if (conn->num_streams.pull.half_closed + conn->num_streams.push.half_closed == 0) {
         conn->_timeout_entry.cb = on_idle_timeout;
         h2o_timeout_link(conn->super.ctx->loop, &conn->super.ctx->http2.idle_timeout, &conn->_timeout_entry);
     }
