63b9d27648807d61237ecddd0176bba52f9f35d6
Ritta Narita <narittan@gmail.com>
2017-06-27 13:56:29 +0900

    Fix Copyright of channel.c

--- lib/handler/mruby/channel.c
+++ lib/handler/mruby/channel.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 DeNA Co., Ltd., Kazuho Oku
+ * Copyright (c) 2017 Ritta Narita
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
