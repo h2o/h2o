9271930bf26ea3209b53db73f66f7c0a12d1a91c
Frederik Deweerdt <fdeweerdt@fastly.com>
2017-07-10 15:27:54 -0700

    debug test

--- t/assets/doc_root/hello.php
+++ t/assets/doc_root/hello.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_GET['link'])) {
-    header('Link: ' . $_GET['link']);
+    //header('Link: ' . $_GET['link']);
+    header('Link: <index.js>; rel=preload');
 }
 echo "hello world";
 ?>
