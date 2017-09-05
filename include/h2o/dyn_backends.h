7757576d37e5c5b6bd541f4dfd58c7db18500048
Frederik Deweerdt <fdeweerdt@fastly.com>
2017-09-05 08:52:54 -0700

    Rename guards

--- include/h2o/dyn_backends.h
+++ include/h2o/dyn_backends.h
@@ -1,5 +1,5 @@
-#ifndef FASTLY_BACKENDS_H_
-#define FASTLY_BACKENDS_H_
+#ifndef DYN_BACKENDS_H_
+#define DYN_BACKENDS_H_
 
 #include "h2o/url.h"
 #include "h2o/socketpool.h"
@@ -10,4 +10,4 @@ typedef struct st_h2o_dyn_backend_config_t {
 const char *h2o_dyn_backend_add(const char *id, h2o_dyn_backend_config_t *config);
 int h2o_dyn_backend_get_upstream(h2o_handler_t *h, h2o_req_t *req, h2o_url_t **upstream, h2o_socketpool_t **pool, void *ctx);
 
-#endif /* FASTLY_BACKENDS_H_ */
+#endif /* DYN_BACKENDS_H_ */
