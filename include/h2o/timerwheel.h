ceab5af3974ece3676fae4241014c3d4b38b8ccd
Grant Zhang <gzhang@fastly.com>
2017-09-20 21:18:37 +0000

    Add basic timerwheel support

---
+++ include/h2o/timerwheel.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2017 Fastly, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#ifndef h2o__timerwheel_h
+#define h2o__timerwheel_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "h2o/linklist.h"
+
+typedef uint64_t wheelmask_t;
+typedef struct st_h2o_timerwheel_timer_t h2o_timerwheel_timer_t;
+typedef void (*h2o_timerwheel_cb)(h2o_timerwheel_timer_t *timer);
+/* link list of h2o_timerwheel_timer_t */
+typedef h2o_linklist_t h2o_timerwheel_slot_t;
+
+#define H2O_TIMERWHEEL_MAX_WHEELS 4
+#define H2O_TIMERWHEEL_BITS_PER_WHEEL 6
+#define H2O_TIMERWHEEL_SLOTS_PER_WHEEL (1 << H2O_TIMERWHEEL_BITS_PER_WHEEL)
+#define H2O_TIMERWHEEL_SLOTS_MASK (H2O_TIMERWHEEL_SLOTS_PER_WHEEL - 1)
+#define H2O_TIMERWHEEL_MAX_TIMER ((1LU << (H2O_TIMERWHEEL_BITS_PER_WHEEL * H2O_TIMERWHEEL_MAX_WHEELS)) - 1)
+
+typedef struct st_h2o_timerwheel_t {
+    h2o_timerwheel_slot_t wheel[H2O_TIMERWHEEL_MAX_WHEELS][H2O_TIMERWHEEL_SLOTS_PER_WHEEL], expired;
+    uint64_t last_run; /* the last time h2o_timerwheel_run was called */
+} h2o_timerwheel_t;
+
+/**
+ * Modules willing to use timers should embed this object as part of itself,
+ * and link it to timer wheel slot.
+ */
+struct st_h2o_timerwheel_timer_t {
+    h2o_linklist_t _link;
+    h2o_timerwheel_cb cb;
+    uint64_t expire_at; /* absolute expiration time*/
+};
+
+/**
+ * initializes a timerwheel
+ */
+void h2o_timerwheel_init(h2o_timerwheel_t *wheel);
+/**
+ * run the timerwheel to absolute time "now"
+ */
+size_t h2o_timerwheel_run(h2o_timerwheel_t *wheel, uint64_t now);
+/**
+ * display the contents of the timerwheel
+ */
+void h2o_timerwheel_show(h2o_timerwheel_t *wheel);
+
+/**
+ * creates a timer
+ */
+h2o_timerwheel_timer_t *h2o_timerwheel_create_timer(h2o_timerwheel_cb cb);
+/**
+ * initializes a timer
+ */
+void h2o_timerwheel_init_timer(h2o_timerwheel_timer_t *t, h2o_timerwheel_cb cb);
+/**
+ * adds a timer to a timerwheel
+ */
+int h2o_timerwheel_add_timer(h2o_timerwheel_t *w, h2o_timerwheel_timer_t *timer, uint64_t abs_expire);
+/**
+ * cancels an existing timer
+ */
+void h2o_timerwheel_del_timer(h2o_timerwheel_timer_t *timer);
+
+int h2o_timer_is_linked(h2o_timerwheel_timer_t *timer);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ t/00unit/lib/common/timerwheel.c
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2017 Fastly, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#include <inttypes.h>
+#include "../../test.h"
+#include "../../../../include/h2o/timerwheel.h"
+
+int invokes = 0;
+static void my_callback(h2o_timerwheel_timer_t *timer) {
+    invokes++;
+}
+
+int rseed = 13;
+
+static inline int lcg_rand() {
+    return rseed = (rseed * 1103515245 + 12345) & RAND_MAX;
+}
+
+#define N 14000
+void test_add_fixed_timers() {
+    int i;
+    h2o_timerwheel_t * testwheel= calloc(1, sizeof(h2o_timerwheel_t));
+    h2o_timerwheel_init(testwheel);
+
+    uint64_t abs_wtime = 3;
+    h2o_timerwheel_timer_t* timers[N];
+    /* add timers */
+    for (i=0; i < N; i++) {
+        uint64_t exp = abs_wtime + i + 5;
+        timers[i] = h2o_timerwheel_create_timer(my_callback);
+        h2o_timerwheel_add_timer(testwheel, timers[i], exp);
+    }
+
+    /* run the wheel */
+    ok(h2o_timerwheel_run(testwheel, 139)==132);
+    h2o_timerwheel_show(testwheel);
+}
+
+void test_del_timers() {
+    int i;
+    h2o_timerwheel_t * testwheel= calloc(1, sizeof(h2o_timerwheel_t));
+    h2o_timerwheel_init(testwheel);
+
+    uint64_t abs_wtime = 3;
+    h2o_timerwheel_timer_t* timers[N];
+    /* add N timers */
+    for (i=0; i < N; i++) {
+        uint64_t exp = abs_wtime + i + 5;
+        timers[i] = h2o_timerwheel_create_timer(my_callback);
+        h2o_timerwheel_add_timer(testwheel, timers[i], exp);
+    }
+
+    /* delete N-1 timers, so there should be 1 timer left */
+    for (i=0; i < N-1; i++) {
+        h2o_timerwheel_del_timer(timers[i]);
+    }
+
+    /* run the wheel */
+    ok(h2o_timerwheel_run(testwheel, N+6)==0);
+    h2o_timerwheel_show(testwheel);
+    ok(h2o_timerwheel_run(testwheel, N+7)==1);
+    h2o_timerwheel_show(testwheel);
+}
+
+void test_add_rand_timers() {
+    int i;
+    h2o_timerwheel_t * testwheel= calloc(1, sizeof(h2o_timerwheel_t));
+    h2o_timerwheel_init(testwheel);
+
+    uint64_t abs_wtime = 3;
+    h2o_timerwheel_timer_t* timers[N];
+    /* add timers */
+    for (i=0; i < N; i++) {
+        uint64_t exp = abs_wtime + lcg_rand()%N;
+        timers[i] = h2o_timerwheel_create_timer(my_callback);
+        h2o_timerwheel_add_timer(testwheel, timers[i], exp);
+    }
+
+    int start = invokes;
+    /* run the wheel: the timers has a max expiry N-1 + abs_wtime  */
+    ok(h2o_timerwheel_run(testwheel, N-1+abs_wtime)==N);
+    ok(invokes - start == N);
+    h2o_timerwheel_show(testwheel);
+}
+
+void test_invalid_timer() {
+    h2o_timerwheel_t * testwheel= calloc(1, sizeof(h2o_timerwheel_t));
+    h2o_timerwheel_init(testwheel);
+
+    h2o_timerwheel_timer_t timer;
+    h2o_timerwheel_init_timer(&timer, my_callback);
+    testwheel->last_run = 3;
+    uint64_t exp = 0x100000007LL + testwheel->last_run;
+    ok(h2o_timerwheel_add_timer(testwheel, &timer, exp) == -1);
+
+    /* stress testing with one million outstanding timers */
+#define NTIMERS 1024*16
+    h2o_timerwheel_timer_t *arr = calloc(NTIMERS, sizeof(h2o_timerwheel_timer_t));
+    exp = 11;
+    int i;
+    for (i=0; i<NTIMERS; i++) {
+        h2o_timerwheel_init_timer(&arr[i], my_callback);
+        assert(h2o_timerwheel_add_timer(testwheel, &arr[i], exp) == 0);
+        exp++;
+    }
+
+    exp = 11;
+    for (i=0; i< NTIMERS; i++) {
+        assert(h2o_timerwheel_run(testwheel, exp++)==1);
+    }
+}
+
+void test_lib__common__timerwheel_c() {
+    subtest("add fixed timers", test_add_fixed_timers);
+    subtest("add random timers", test_add_rand_timers);
+    subtest("del fixed timers", test_del_timers);
+    subtest("test out-of-range timer", test_invalid_timer);
+}
