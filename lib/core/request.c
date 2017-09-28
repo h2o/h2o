a4a1b3f2afcda6cb1edad9ffb8ce70ef47f08eb5
Kazuho Oku <kazuhooku@gmail.com>
2017-09-28 18:37:32 +0900

    support `critical` attribute (https://github.com/w3c/preload/issues/66)

--- lib/core/request.c
+++ lib/core/request.c
@@ -697,21 +697,21 @@ h2o_iovec_t h2o_get_redirect_method(h2o_iovec_t method, int status)
     return method;
 }
 
+static void do_push_path(void *_req, const char *path, size_t path_len, int is_critical)
+{
+    h2o_req_t *req = _req;
+    req->conn->callbacks->push_path(req, path, path_len, is_critical);
+}
+
 h2o_iovec_t h2o_push_path_in_link_header(h2o_req_t *req, const char *value, size_t value_len)
 {
-    int i;
     h2o_iovec_t ret = h2o_iovec_init(value, value_len);
     if (req->conn->callbacks->push_path == NULL)
         return ret;
 
-    h2o_iovec_vector_t paths = h2o_extract_push_path_from_link_header(
-        &req->pool, value, value_len, req->path_normalized, req->input.scheme, req->input.authority,
-        req->res_is_delegated ? req->scheme : NULL, req->res_is_delegated ? &req->authority : NULL, &ret);
-    if (paths.size == 0)
-        return ret;
+    h2o_extract_push_path_from_link_header(&req->pool, value, value_len, req->path_normalized, req->input.scheme,
+                                           req->input.authority, req->res_is_delegated ? req->scheme : NULL,
+                                           req->res_is_delegated ? &req->authority : NULL, do_push_path, req, &ret);
 
-    for (i = 0; i < paths.size; i++) {
-        req->conn->callbacks->push_path(req, paths.entries[i].base, paths.entries[i].len);
-    }
     return ret;
 }
