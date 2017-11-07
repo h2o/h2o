4bca5ebed60e191f3772fe3aa82a737153ffb5f9
Kazuho Oku <kazuhooku@gmail.com>
2017-11-07 15:08:43 +0900

    add missing stub

--- t/00unit/test.c
+++ t/00unit/test.c
@@ -59,9 +59,14 @@ static socklen_t get_peername(h2o_conn_t *conn, struct sockaddr *sa)
     return sizeof(*sin);
 }
 
+static h2o_socket_t *get_socket(h2o_conn_t *conn)
+{
+    return NULL;
+}
+
 h2o_loopback_conn_t *h2o_loopback_create(h2o_context_t *ctx, h2o_hostconf_t **hosts)
 {
-    static const h2o_conn_callbacks_t callbacks = {get_sockname, get_peername};
+    static const h2o_conn_callbacks_t callbacks = {get_sockname, get_peername, NULL, get_socket};
     h2o_loopback_conn_t *conn = (void *)h2o_create_connection(sizeof(*conn), ctx, hosts, (struct timeval){0}, &callbacks);
 
     memset((char *)conn + sizeof(conn->super), 0, offsetof(struct st_h2o_loopback_conn_t, req) - sizeof(conn->super));
