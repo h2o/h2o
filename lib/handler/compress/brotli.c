b76ca85f2d9c009e4793840b109532f7c84b10ac
Kazuho Oku <kazuhooku@gmail.com>
2017-12-07 05:33:31 +0900

    update copyright reflecting the complete rewrite

--- lib/handler/compress/brotli.c
+++ lib/handler/compress/brotli.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 DeNA Co., Ltd., Kazuho Oku
+ * Copyright (c) 2017 Fastly, Kazuho Oku
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
