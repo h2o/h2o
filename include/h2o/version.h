42934e6f3455ca90af00d20590e881b591b0b415
Kazuho Oku <kazuhooku@gmail.com>
2017-12-15 12:05:31 +0900

    releng for 2.2.4

--- include/h2o/version.h
+++ include/h2o/version.h
@@ -22,14 +22,14 @@
 #ifndef h2o__version_h
 #define h2o__version_h
 
-#define H2O_VERSION "2.2.3"
+#define H2O_VERSION "2.2.4"
 
 #define H2O_VERSION_MAJOR 2
 #define H2O_VERSION_MINOR 2
-#define H2O_VERSION_PATCH 3
+#define H2O_VERSION_PATCH 4
 
 #define H2O_LIBRARY_VERSION_MAJOR 0
 #define H2O_LIBRARY_VERSION_MINOR 13
-#define H2O_LIBRARY_VERSION_PATCH 3
+#define H2O_LIBRARY_VERSION_PATCH 4
 
 #endif
