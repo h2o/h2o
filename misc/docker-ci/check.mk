7dbc457404196e5489fb3fb119e4a70f4c1b4bc3
Kazuho Oku <kazuhooku@gmail.com>
2017-12-18 11:33:02 +0900

    bootstrap from public image

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -5,10 +5,6 @@ ALL:
 	sudo make check-as-root
 
 fuzz:
-	sudo apt-get install -y software-properties-common
-	curl -L http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-	sudo apt-add-repository -y 'deb http://llvm.org/apt/xenial llvm-toolchain-xenial-4.0 main'
-	sudo apt-get install -y clang-4.0
 	sudo ln -s /usr/bin/clang-4.0 /usr/bin/clang
 	sudo ln -s /usr/bin/clang++-4.0 /usr/bin/clang++
 	ASAN_OPTIONS=detect_leaks=0 CC=clang CXX=clang++ make -f misc/docker-ci/check.mk do-fuzz
