dfe688a9285f757b0882b14c816bad63483b5299
Ichito Nagata <i.nagata110@gmail.com>
2017-12-22 16:59:38 +0900

    fix duplicate date headers

--- lib/handler/mruby.c
+++ lib/handler/mruby.c
@@ -799,7 +799,7 @@ static int send_response(h2o_mruby_generator_t *generator, mrb_int status, mrb_v
         return -1;
     }
     /* add date: if it's missing from the response */
-    if (h2o_find_header(&generator->req->res.headers, H2O_TOKEN_DATE, 0) == -1)
+    if (h2o_find_header(&generator->req->res.headers, H2O_TOKEN_DATE, SIZE_MAX) == -1)
         h2o_resp_add_date_header(generator->req);
 
     /* return without processing body, if status is fallthru */
