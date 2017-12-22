f161cdb4d13acece84fbfab27a1d1b388d3e26b5
Kazuho Oku <kazuhooku@gmail.com>
2017-12-22 10:41:11 +0900

    redo 2af64cb

--- deps/yaml/src/yaml_private.h
+++ deps/yaml/src/yaml_private.h
@@ -1,7 +1,8 @@
 
-#if HAVE_CONFIG_H
-#include <config.h>
-#endif
+#define YAML_VERSION_MAJOR 0
+#define YAML_VERSION_MINOR 1
+#define YAML_VERSION_PATCH 7
+#define YAML_VERSION_STRING "0.1.7"
 
 #include <yaml.h>
 
