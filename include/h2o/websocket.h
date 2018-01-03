3a651cdb60dceceea4b3587a51ea2e4facb0830a
Baodong Chen <chenbdchenbd@gmail.com>
2018-01-03 21:35:19 +0800

    remove unsed prototype in "include/h2o/websocket.h"

--- include/h2o/websocket.h
+++ include/h2o/websocket.h
@@ -32,7 +32,7 @@ extern "C" {
 
 typedef struct st_h2o_websocket_conn_t h2o_websocket_conn_t;
 
-/* arg is NULL if the connection has been closed */
+/* if arg is NULL, the user should close connection by calling h2o_websocket_close() */
 typedef void (*h2o_websocket_msg_callback)(h2o_websocket_conn_t *conn, const struct wslay_event_on_msg_recv_arg *arg);
 
 struct st_h2o_websocket_conn_t {
@@ -48,7 +48,6 @@ struct st_h2o_websocket_conn_t {
 };
 
 int h2o_is_websocket_handshake(h2o_req_t *req, const char **client_key);
-void h2o_websocket_create_accept_key(char *dst, const char *client_key);
 h2o_websocket_conn_t *h2o_upgrade_to_websocket(h2o_req_t *req, const char *client_key, void *user_data,
                                                h2o_websocket_msg_callback msg_cb);
 void h2o_websocket_close(h2o_websocket_conn_t *conn);
