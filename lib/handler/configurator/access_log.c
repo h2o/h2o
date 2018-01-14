ee30d9101b5adbd0c2c1e3b3c06d956244f219e4
Kazuho Oku <kazuhooku@gmail.com>
2018-01-14 16:52:28 +0900

    use parse_mappings

--- lib/handler/configurator/access_log.c
+++ lib/handler/configurator/access_log.c
@@ -33,55 +33,46 @@ struct st_h2o_access_log_configurator_t {
 static int on_config(h2o_configurator_command_t *cmd, h2o_configurator_context_t *ctx, yoml_t *node)
 {
     struct st_h2o_access_log_configurator_t *self = (void *)cmd->configurator;
-    const char *path, *fmt = NULL;
+    yoml_t **path, **format = NULL, **escape_node = NULL;
     int escape = H2O_LOGCONF_ESCAPE_APACHE;
     h2o_access_log_filehandle_t *fh;
 
     switch (node->type) {
     case YOML_TYPE_SCALAR:
-        path = node->data.scalar;
+        path = &node;
         break;
-    case YOML_TYPE_MAPPING: {
-        yoml_t *t;
-        /* get path */
-        if ((t = yoml_get(node, "path")) == NULL) {
-            h2o_configurator_errprintf(cmd, node, "could not find mandatory key `path`");
+    case YOML_TYPE_MAPPING:
+        if (h2o_configurator_parse_mapping(cmd, node, "path", "format,escape", &path, &format, &escape_node) != 0)
+            return -1;
+        if ((*path)->type != YOML_TYPE_SCALAR) {
+            h2o_configurator_errprintf(cmd, *path, "`path` must be scalar");
             return -1;
         }
-        if (t->type != YOML_TYPE_SCALAR) {
-            h2o_configurator_errprintf(cmd, t, "`path` must be scalar");
+        if (format != NULL && (*format)->type != YOML_TYPE_SCALAR) {
+            h2o_configurator_errprintf(cmd, *format, "`format` must be a scalar");
             return -1;
         }
-        path = t->data.scalar;
-        /* get format */
-        if ((t = yoml_get(node, "format")) != NULL) {
-            if (t->type != YOML_TYPE_SCALAR) {
-                h2o_configurator_errprintf(cmd, t, "`format` must be a scalar");
-                return -1;
-            }
-            fmt = t->data.scalar;
-        }
-        /* get escape */
-        if ((t = yoml_get(node, "escape")) != NULL) {
-            switch (h2o_configurator_get_one_of(cmd, t, "apache,json")) {
-            case 0:
-                escape = H2O_LOGCONF_ESCAPE_APACHE;
-                break;
-            case 1:
-                escape = H2O_LOGCONF_ESCAPE_JSON;
-                break;
-            default:
-                return -1;
-            }
-        }
-    } break;
+        break;
     default:
         h2o_configurator_errprintf(cmd, node, "node must be a scalar or a mapping");
         return -1;
     }
 
+    if (escape_node != NULL) {
+        switch (h2o_configurator_get_one_of(cmd, *escape_node, "apache,json")) {
+        case 0:
+            escape = H2O_LOGCONF_ESCAPE_APACHE;
+            break;
+        case 1:
+            escape = H2O_LOGCONF_ESCAPE_JSON;
+            break;
+        default:
+            return -1;
+        }
+    }
+
     if (!ctx->dry_run) {
-        if ((fh = h2o_access_log_open_handle(path, fmt, escape)) == NULL)
+        if ((fh = h2o_access_log_open_handle((*path)->data.scalar, format != NULL ? (*format)->data.scalar : NULL, escape)) == NULL)
             return -1;
         h2o_vector_reserve(NULL, self->handles, self->handles->size + 1);
         self->handles->entries[self->handles->size++] = fh;
