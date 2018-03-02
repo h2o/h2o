84b9c33f286d0865ea5c617d3e2be54bd9d256ec
Ichito Nagata <i.nagata110@gmail.com>
2018-03-02 14:24:13 +0900

    remove git revision if it's tagged

--- misc/generate_gitrev.sh
+++ misc/generate_gitrev.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 GITREV=$(git rev-parse --short HEAD)
-CONTENT="#define H2O_GIT_REVISION $GITREV"
+GITTAG=$(git tag --points-at ${GITREV})
+CONTENT=""
+if [ -z "$GITTAG" ]; then
+    CONTENT="#define H2O_GIT_REVISION $GITREV"
+fi
 OUTPATH="include/h2o/gitrev.h"
 if [ ! -f $OUTPATH ] || [ "$CONTENT" != "$(cat $OUTPATH)" ]; then
     echo "$CONTENT" > $OUTPATH
