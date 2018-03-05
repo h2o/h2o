71655bc8abd69d7c909c30c8eda2af65037bfb4c
Ichito Nagata <i.nagata110@gmail.com>
2018-03-05 15:52:51 +0900

    make the names conform to our convention

--- misc/generate_gitrev.sh
+++ misc/generate_gitrev.sh
@@ -3,7 +3,7 @@ GITREV=$(git rev-parse --short HEAD)
 GITTAG=$(git tag --points-at ${GITREV})
 CONTENT=""
 if [ -z "$GITTAG" ]; then
-    CONTENT="#define H2O_GIT_REVISION $GITREV"
+    CONTENT="#define H2O_GITREV $GITREV"
 fi
 OUTPATH="include/h2o/gitrev.h"
 if [ ! -f $OUTPATH ] || [ "$CONTENT" != "$(cat $OUTPATH)" ]; then
