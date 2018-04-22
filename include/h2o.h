1081a9fb96b9e719ee58002116ad046321f1b215
Ichito Nagata <i.nagata110@gmail.com>
2018-04-22 22:09:21 +0900

    Revert "move h2o_header_t definition to header.h"
    This reverts commit 5e170f0c17a6bc6dee353beae1ca39cb62c50ec3.

--- include/h2o.h
+++ include/h2o.h
@@ -52,7 +52,6 @@ extern "C" {
 #include "h2o/url.h"
 #include "h2o/version.h"
 #include "h2o/balancer.h"
-#include "h2o/header.h"
 
 #ifndef H2O_USE_BROTLI
 /* disabled for all but the standalone server, since the encoder is written in C++ */
@@ -676,6 +675,29 @@ struct st_h2o_context_t {
 
     H2O_VECTOR(h2o_pathconf_t *) _pathconfs_inited;
 };
+
+/**
+ * represents a HTTP header
+ */
+typedef struct st_h2o_header_t {
+    /**
+     * name of the header (may point to h2o_token_t which is an optimized subclass of h2o_iovec_t)
+     */
+    h2o_iovec_t *name;
+    /**
+     * The name of the header as originally received from the client, same length as `name`
+     */
+    const char *orig_name;
+    /**
+     * value of the header
+     */
+    h2o_iovec_t value;
+} h2o_header_t;
+
+/**
+ * list of headers
+ */
+typedef H2O_VECTOR(h2o_header_t) h2o_headers_t;
 
 /**
  * an object that generates a response.
