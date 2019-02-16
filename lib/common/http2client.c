d85be28c254925671c802d520bae16a26a8bad73
Baodong Chen <chenbdchenbd@gmail.com>
2019-02-16 11:21:51 +0800

    add 'const' for httpclient1|2 struct size global variable

--- lib/common/http2client.c
+++ lib/common/http2client.c
@@ -1297,4 +1297,4 @@ void h2o_httpclient__h2_on_connect(h2o_httpclient_t *_client, h2o_socket_t *sock
     on_connection_ready(stream, conn);
 }
 
-size_t h2o_httpclient__h2_size = sizeof(struct st_h2o_http2client_stream_t);
+const size_t h2o_httpclient__h2_size = sizeof(struct st_h2o_http2client_stream_t);
