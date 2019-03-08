8f27b03edecf60783c99c5b5a1156eb0b81570e0
Kazuho Oku <kazuhooku@gmail.com>
2019-03-09 04:27:03 +0900

    unsigned is `%u`, use hard-coded numbers rather than TYPE_MAX that depends on the enviroment, avoid invalid read on stack due to sizeof(sockaddr_storage) > sizeof(sockaddr_in)

--- lib/handler/mruby/middleware.c
+++ lib/handler/mruby/middleware.c
@@ -313,19 +313,16 @@ static socklen_t parse_hostport(h2o_mem_pool_t *pool, h2o_iovec_t host, h2o_iove
 {
     /* fast path for IPv4 addresses */
     {
-        unsigned int d1, d2, d3, d4, _port;
+        unsigned d1, d2, d3, d4, _port;
         int parsed_len;
-        if (sscanf(host.base, "%" SCNd32 "%*[.]%" SCNd32 "%*[.]%" SCNd32 "%*[.]%" SCNd32 "%n", &d1, &d2, &d3, &d4, &parsed_len) ==
-                4 &&
-            parsed_len == host.len && d1 <= UCHAR_MAX && d2 <= UCHAR_MAX && d3 <= UCHAR_MAX && d4 <= UCHAR_MAX) {
-            if (sscanf(port.base, "%" SCNd32 "%n", &_port, &parsed_len) == 1 && parsed_len == port.len && _port <= USHRT_MAX) {
-                struct sockaddr_in sin;
-                memset(&sin, 0, sizeof(sin));
-                sin.sin_family = AF_INET;
-                sin.sin_port = htons(_port);
-                sin.sin_addr.s_addr = ntohl((d1 << 24) + (d2 << 16) + (d3 << 8) + d4);
-                *ss = *((struct sockaddr_storage *)&sin);
-                return sizeof(sin);
+        if (sscanf(host.base, "%u%*[.]%u%*[.]%u%*[.]%u%n", &d1, &d2, &d3, &d4, &parsed_len) == 4 && parsed_len == host.len &&
+            d1 <= 255 && d2 <= 255 && d3 <= 255 && d4 <= 255) {
+            if (sscanf(port.base, "%u%n", &_port, &parsed_len) == 1 && parsed_len == port.len && _port <= 65535) {
+                struct sockaddr_in *sin = (void *)ss;
+                sin->sin_family = AF_INET;
+                sin->sin_port = htons(_port);
+                sin->sin_addr.s_addr = ntohl((d1 << 24) + (d2 << 16) + (d3 << 8) + d4);
+                return sizeof(*sin);
             }
         }
     }
