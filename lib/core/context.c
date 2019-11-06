7fbc71a906786777b35ce6f4f571ca04886e3d66
Frederik Deweerdt <fdeweerdt@fastly.com>
2019-11-06 15:18:32 -0800

    Make sure several threads are not accessing the timerwheel
    We ensure this by only letting one thread update the timers: the first
    one that called `h2o_socketpool_register_loop`. The other threads can
    still expire sockets, just not adjust the timer for the next expiration.

--- lib/core/context.c
+++ lib/core/context.c
@@ -113,9 +113,13 @@ void h2o_context_init(h2o_context_t *ctx, h2o_loop_t *loop, h2o_globalconf_t *co
     memset(ctx->_module_configs, 0, sizeof(*ctx->_module_configs) * config->_num_config_slots);
 
     static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    static int once = 0;
     pthread_mutex_lock(&mutex);
 
-    h2o_socketpool_register_loop(&ctx->globalconf->proxy.global_socketpool, loop);
+    if (once == 0) {
+        once++;
+        h2o_socketpool_register_loop(&ctx->globalconf->proxy.global_socketpool, loop);
+    }
 
     for (i = 0; config->hosts[i] != NULL; ++i) {
         h2o_hostconf_t *hostconf = config->hosts[i];
