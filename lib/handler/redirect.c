508f5b3f12bb37affaba40283981a73b1565e955
Ichito Nagata <i.nagata110@gmail.com>
2019-12-02 17:05:04 +0900

    use iovec list to build prefix instead of char pointers

--- lib/handler/redirect.c
+++ lib/handler/redirect.c
@@ -30,16 +30,18 @@ struct st_h2o_redirect_handler_t {
     h2o_handler_t super;
     int internal;
     int status;
-    h2o_iovec_t prefix;
-    char_vec wildcard_chars;
+    h2o_iovec_vector_t prefix_list;
 };
 
 static void on_dispose(h2o_handler_t *_self)
 {
     h2o_redirect_handler_t *self = (void *)_self;
-    free(self->prefix.base);
-    if (self->wildcard_chars.entries != NULL) {
-        free(self->wildcard_chars.entries);
+    if (self->prefix_list.entries != NULL) {
+        size_t i;
+        for (i = 0; i != self->prefix_list.size; ++i) {
+            free(self->prefix_list.entries[i].base);
+        }
+        free(self->prefix_list.entries);
     }
 }
 
@@ -85,38 +87,49 @@ SendInternalError:
     h2o_send_error_503(req, "Internal Server Error", "internal server error", 0);
 }
 
-static h2o_iovec_t replace_wildcard(h2o_mem_pool_t *pool, h2o_iovec_t target, char_vec *wildcard_chars, h2o_iovec_t replacement)
+static h2o_iovec_t join_list(h2o_mem_pool_t *pool, h2o_iovec_vector_t *list, h2o_iovec_t delimiter)
 {
-    h2o_iovec_t replaced;
-    replaced.len = target.len + wildcard_chars->size * (replacement.len - 1);
-    replaced.base = h2o_mem_alloc_pool(pool, char, replaced.len);
-    char *src = target.base;
-    char *dst = replaced.base;
+    h2o_iovec_vector_t joined = (h2o_iovec_vector_t){ 0 };
+
+    assert(list->size > 0);
+    h2o_vector_reserve(pool, &joined, list->size * 2 - 1);
+
     size_t i;
-    for (i = 0; i != wildcard_chars->size; ++i) {
-        size_t plen = wildcard_chars->entries[i] - src;
-        memcpy(dst, src, plen);
-        dst += plen;
-        src += plen;
-        memcpy(dst, replacement.base, replacement.len);
-        dst += replacement.len;
-        src += 1;
+    for (i = 0; i != list->size; ++i) {
+        if (i != 0) {
+            joined.entries[joined.size++] = delimiter;
+        }
+        joined.entries[joined.size++] = list->entries[i];
     }
-    memcpy(dst, src, target.len - (src - target.base));
-    return replaced;
+    return h2o_concat_list(pool, joined.entries, joined.size);
+}
+
+static void build_prefix_list(h2o_iovec_t str, h2o_iovec_vector_t *dest)
+{
+    *dest = (h2o_iovec_vector_t){ 0 };
+    if (str.base == NULL) {
+        return;
+    }
+    char *start = str.base;
+    char *end = start + str.len;
+    char *p = start;
+    char *found;
+    while (p < end && (found = memchr(p, '*', end - p)) != NULL) {
+        h2o_vector_reserve(NULL, dest, dest->size + 1);
+        dest->entries[dest->size++] = h2o_strdup(NULL, p, found - p);;
+        p = found + 1;
+    }
+    assert(p <= end);
+    h2o_vector_reserve(NULL, dest, dest->size + 1);
+    dest->entries[dest->size++] = h2o_strdup(NULL, p, end - p);
 }
 
 static int on_req(h2o_handler_t *_self, h2o_req_t *req)
 {
     h2o_redirect_handler_t *self = (void *)_self;
 
-    h2o_iovec_t prefix;
-    if (self->wildcard_chars.entries != NULL && req->authority_wildcard_match.base != NULL) {
-        prefix = replace_wildcard(&req->pool, self->prefix, &self->wildcard_chars, req->authority_wildcard_match);
-    } else {
-        prefix = self->prefix;
-    }
-
+    h2o_iovec_t delimiter = req->authority_wildcard_match.base == NULL ? h2o_iovec_init(H2O_STRLIT("*")) : req->authority_wildcard_match;
+    h2o_iovec_t prefix = join_list(&req->pool, &self->prefix_list, delimiter);
     h2o_iovec_t dest = h2o_build_destination(req, prefix.base, prefix.len, 1);
 
     /* redirect */
@@ -129,22 +142,6 @@ static int on_req(h2o_handler_t *_self, h2o_req_t *req)
     return 0;
 }
 
-static void collect_wildcard_chars(h2o_iovec_t str, char_vec *dest)
-{
-    *dest = (char_vec){ 0 };
-    if (str.base == NULL) {
-        return;
-    }
-    char *start = str.base;
-    char *end = start + str.len;
-    char *p = start;
-    while (p < end && (p = memchr(p, '*', end - p)) != NULL) {
-        h2o_vector_reserve(NULL, dest, dest->size + 1);
-        dest->entries[dest->size++] = p;
-        ++p;
-    }
-}
-
 h2o_redirect_handler_t *h2o_redirect_register(h2o_pathconf_t *pathconf, int internal, int status, const char *prefix)
 {
     h2o_redirect_handler_t *self = (void *)h2o_create_handler(pathconf, sizeof(*self));
@@ -152,8 +149,7 @@ h2o_redirect_handler_t *h2o_redirect_register(h2o_pathconf_t *pathconf, int inte
     self->super.on_req = on_req;
     self->internal = internal;
     self->status = status;
-    self->prefix = h2o_strdup(NULL, prefix, SIZE_MAX);
-    collect_wildcard_chars(self->prefix, &self->wildcard_chars);
+    build_prefix_list(h2o_iovec_init(prefix, strlen(prefix)), &self->prefix_list);
 
     return self;
 }
