eab8df2c47cf0a1692b905d82b64efeb6df2b953
Hajime Fujita <crisp.fujita@nifty.com>
2019-12-20 16:55:21 -0600

    Add a unit test for absolute priority header parser

--- t/00unit/test.h
+++ t/00unit/test.h
@@ -60,6 +60,7 @@ void test_lib__common__string_c(void);
 void test_lib__common__time_c(void);
 void test_lib__common__timerwheel_c(void);
 void test_lib__common__url_c(void);
+void test_lib__common__absprio_c(void);
 void test_lib__core__headers_c(void);
 void test_lib__core__proxy_c(void);
 void test_lib__core__util_c(void);
