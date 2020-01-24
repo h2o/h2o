ef80e1f365463a20552ff2e3fde2372c49665d99
Toru Maesaka <toru@torumk.com>
2020-01-24 00:59:57 +0000

    h2olog: remove redundant pid argument

--- h2olog.py
+++ h2olog.py
@@ -99,7 +99,7 @@ def usage():
     print ("USAGE: h2olog -p PID")
     exit()
 
-def trace_http(pid):
+def trace_http():
     b["rxbuf"].open_perf_buffer(handle_req_line)
     b["txbuf"].open_perf_buffer(handle_resp_line)
 
@@ -127,9 +127,10 @@ if h2o_pid == 0:
 
 u = USDT(pid=int(h2o_pid))
 if sys.argv[1] != "quic":
+    tracer_func = trace_http
     u.enable_probe(probe="receive_request", fn_name="trace_receive_req")
     u.enable_probe(probe="receive_request_header", fn_name="trace_receive_req_header")
     u.enable_probe(probe="send_response", fn_name="trace_send_resp")
 
 b = BPF(text=bpf, usdt_contexts=[u])
-trace_http(h2o_pid)
+tracer_func()
