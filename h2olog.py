90daeec6992e91af987d39a65339f31f74fe95f9
Toru Maesaka <toru@torumk.com>
2020-02-01 04:47:11 +0000

    h2olog/quic: support the streams_blocked_send probe

--- h2olog.py
+++ h2olog.py
@@ -112,6 +112,8 @@ struct quic_event_t {
     u32 new_version;
     u64 len;
     u64 token_generation;
+    u64 limit;
+    u32 is_unidirectional;
 };
 
 BPF_PERF_OUTPUT(events);
@@ -382,6 +384,25 @@ int trace_new_token_receive(struct pt_regs *ctx) {
 
     return 0;
 }
+
+int trace_streams_blocked_send(struct pt_regs *ctx) {
+    void *pos = NULL;
+    struct quic_event_t event = {};
+    struct st_quicly_conn_t conn = {};
+    sprintf(event.type, "handshake_done_send");
+
+    bpf_usdt_readarg(1, ctx, &pos);
+    bpf_probe_read(&conn, sizeof(conn), pos);
+    event.master_conn_id = conn.master_id;
+    bpf_usdt_readarg(2, ctx, &event.at);
+    bpf_usdt_readarg(3, ctx, &event.limit);
+    bpf_usdt_readarg(4, ctx, &event.is_unidirectional);
+
+    if (events.perf_submit(ctx, &event, sizeof(event)) < 0)
+        bpf_trace_printk("failed to perf_submit\\n");
+
+    return 0;
+}
 """
 
 def handle_req_line(cpu, data, size):
@@ -506,6 +527,7 @@ if sys.argv[1] == "quic":
     u.enable_probe(probe="new_token_send", fn_name="trace_new_token_send")
     u.enable_probe(probe="new_token_acked", fn_name="trace_new_token_acked")
     u.enable_probe(probe="new_token_receive", fn_name="trace_new_token_receive")
+    u.enable_probe(probe="streams_blocked_send", fn_name="trace_streams_blocked_send")
     b = BPF(text=quic_bpf, usdt_contexts=[u])
 else:
     u.enable_probe(probe="receive_request", fn_name="trace_receive_req")
