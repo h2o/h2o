4fd7815296201af30d99d1fc70f21befd0b02236
Ichito Nagata <i.nagata110@gmail.com>
2020-02-06 02:34:58 +0900

    add an offset when retrieving corresponding pathconf because there may already be other paths (ex: path for http2-debug-state)

--- lib/core/configurator.c
+++ lib/core/configurator.c
@@ -331,6 +331,7 @@ static int on_config_paths(h2o_configurator_command_t *cmd, h2o_configurator_con
           (int (*)(const void *, const void *))sort_from_longer_paths);
     
     /* At first regsiter all paths before configure, because hostconf->paths may be reallocated */
+    size_t prev_paths_size = ctx->hostconf->paths.size;
     for (i = 0; i != node->data.mapping.size; ++i) {
         yoml_t *key = node->data.mapping.elements[i].key;
         h2o_config_register_path(ctx->hostconf, key->data.scalar, 0);
@@ -341,7 +342,7 @@ static int on_config_paths(h2o_configurator_command_t *cmd, h2o_configurator_con
         yoml_t *value;
         if ((value = convert_path_config_node(cmd, node->data.mapping.elements[i].value)) == NULL)
             return -1;
-        h2o_pathconf_t *pathconf = ctx->hostconf->paths.entries + i;
+        h2o_pathconf_t *pathconf = ctx->hostconf->paths.entries + prev_paths_size + i;
         int cmd_ret = config_path(ctx, pathconf, value);
         yoml_free(value, NULL);
         if (cmd_ret != 0)
