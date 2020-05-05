14bff950548aebcd964196a29cbb4337d67976fd
Toru Maesaka <toru@torumk.com>
2020-05-05 02:37:51 +0000

    for consistency with generated-quic.cc

--- misc/gen-quic-bpf.py
+++ misc/gen-quic-bpf.py
@@ -20,7 +20,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-# usage: gen-bpf.py d_files_dir output_file
+# usage: gen-quic-bpf.py d_files_dir output_file
 
 import re
 import sys
