aad9c52f78c664617f21af174e06705e33a6337b
FUJI Goro <goro@fastly.com>
2020-08-11 16:24:40 +0900

    improve the function doc for h2o_file_mktemp()
    Co-authored-by: Kazuho Oku <kazuhooku@gmail.com>

--- include/h2o/file.h
+++ include/h2o/file.h
@@ -28,7 +28,7 @@ h2o_iovec_t h2o_file_read(const char *fn);
 
 /**
  * creates a temporary file using the fn_template param.
- * This is a wrapper to mkstemp(3), but there's no way to access the actual filename.
+ * This is a wrapper to mkstemp(3), but the file is unlinked before returning from the function. Therefore, the name of the file is not provided to the caller.
  * @return fd. -1 on failure and set errno as mkstemp(3) does.
  */
 int h2o_file_mktemp(const char *fn_template);
