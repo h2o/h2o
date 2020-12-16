f1cac5255d7effe4f732425711343c42590ddade
FUJI Goro <goro@fastly.com>
2020-12-16 12:55:10 +0900

    Revert "ci: redirect stderr into stdout in check.mk"
    This reverts commit bca1d12f52a1cef896cc3e1a3a98c4ed2d689638.

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -27,7 +27,7 @@ _check:
 	sudo mount -t tmpfs tmpfs build -o size=3G
 	sudo chown -R ci:ci build
 	sudo chmod 0755 build
-	exec 2>&1 ; $(MAKE) -f $(CHECK_MK) -C build _do-check CMAKE_ARGS=$(CMAKE_ARGS)
+	$(MAKE) -f $(CHECK_MK) -C build _do-check CMAKE_ARGS=$(CMAKE_ARGS)
 
 _do-check:
 	cmake $(CMAKE_ARGS) -H$(SRC_DIR) -B.
