ddf0833b1b2b0bd1d08a244f0207e87a0ad943be
Goro Fuji <goro@fastly.com>
2020-12-23 01:46:30 +0000

    ci/dtrace: bind Linux kernel stuff for BCC

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -3,7 +3,14 @@ SRC_DIR=/h2o
 CHECK_MK=$(SRC_DIR)/misc/docker-ci/check.mk
 CMAKE_ARGS=
 FUZZ_ASAN=ASAN_OPTIONS=detect_leaks=0
-DOCKER_RUN_OPTS=--privileged -v `pwd`:$(SRC_DIR) -v /sys/kernel/debug:/sys/kernel/debug --add-host=127.0.0.1.xip.io:127.0.0.1 -it
+DOCKER_RUN_OPTS=--privileged \
+	-v `pwd`:$(SRC_DIR) \
+	-v /sys/kernel/debug:/sys/kernel/debug \
+	-v /lib/modules:/lib/modules:ro \
+	-v /usr/src:/usr/src:ro \
+	-v /usr/include/linux:/usr/include/linux:ro \
+	--add-host=127.0.0.1.xip.io:127.0.0.1 \
+	-it
 
 ALL:
 	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) make -f $(SRC_DIR)/misc/docker-ci/check.mk _check
