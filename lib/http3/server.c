21cae2d6582b70299eecd154f4ba0d2e21fa8d2f
Frederik Deweerdt <fdeweerdt@fastly.com>
2021-02-22 13:08:00 -0800

    In CONNECT tunnels, the `datagram_flow_id` is `UINT64_MAX`, take that into account

--- lib/http3/server.c
+++ lib/http3/server.c
@@ -394,9 +394,11 @@ static void destroy_tunnel(struct st_h2o_http3_server_stream_t *stream)
 
     /* remove stream from datagram flow list */
     struct st_h2o_http3_server_conn_t *conn = get_conn(stream);
-    khiter_t iter = kh_get(stream, conn->datagram_flows, stream->tunnel->datagram_flow_id);
-    assert(iter != kh_end(conn->datagram_flows));
-    kh_del(stream, conn->datagram_flows, iter);
+    if (stream->tunnel->datagram_flow_id != UINT64_MAX) {
+        khiter_t iter = kh_get(stream, conn->datagram_flows, stream->tunnel->datagram_flow_id);
+        assert(iter != kh_end(conn->datagram_flows));
+        kh_del(stream, conn->datagram_flows, iter);
+    }
 }
 
 static void dispose_request(struct st_h2o_http3_server_stream_t *stream)
