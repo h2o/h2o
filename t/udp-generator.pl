57c8f8d8f452fb33c71e4c0596b4bf69dd7f1b0a
Hajime Fujita <crisp.fujita@nifty.com>
2021-03-15 17:45:51 +0000

    Rewrite udp-generator.py using Perl
    This will remove dependency to Python.

---
+++ t/udp-generator.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+# Read datagrams from input files and send them to a specified server
+
+use strict;
+use IO::File;
+use IO::Socket;
+
+if (scalar @ARGV < 3) {
+	print STDERR "Usage: $0 [server] [port] [input...]\n";
+	exit 1;
+}
+
+my $server = shift @ARGV;
+my $port = shift @ARGV;
+
+my $sock = IO::Socket::INET->new(
+	Type     => SOCK_DGRAM,
+	Proto    => 'udp',
+	PeerAddr => $server,
+	PeerPort => $port,
+) or die "Failed to create a UDP socket: $!\n";
+
+foreach my $f (@ARGV) {
+	my $fh = IO::File->new($f, "r") or die "Failed to open file: $!\n";
+	my $read = $fh->read(my $dgram, 1500); # 1500 bytes must be enough to store the entire datagram at the moment
+	undef $fh;
+	my $sent = $sock->send($dgram) or die "Failed to send a datagram: $!\n";
+	if ($read != $sent) {
+		die "$read bytes read but $sent bytes sent\n";
+	}
+}
+
+$sock->close();
