bed23ed260eceb4321a67da55023e1d4aa861ccf
FUJI Goro <goro@fastly.com>
2021-03-23 14:39:20 +0900

    build quicly/cli in h2o

--- misc/quic-interop-runner/deploy.sh
+++ misc/quic-interop-runner/deploy.sh
@@ -1,2 +1,5 @@
-docker build -t h2oserver/quicly-interop-runner:latest . --build-arg CACHEBUST=$(date +%s)
+#!/bin/sh
+set -xe
+DIRNAME=$(dirname "$0")
+docker build -t h2oserver/quicly-interop-runner:latest -f $DIRNAME/Dockerfile . --build-arg CACHEBUST=$(date +%s)
 docker push h2oserver/quicly-interop-runner:latest
