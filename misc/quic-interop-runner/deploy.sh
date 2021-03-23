6ad382b6a16c6f35ffae79dbd1b3b259d59170eb
FUJI Goro <goro@fastly.com>
2021-03-23 15:01:54 +0900

    quic-interop-runner: add CI setting for building and deploying h2o images for QUIC Interop Runner

--- misc/quic-interop-runner/deploy.sh
+++ misc/quic-interop-runner/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -xe
 DIRNAME=$(dirname "$0")
-docker build -t h2oserver/quicly-interop-runner:latest -f $DIRNAME/Dockerfile . --build-arg CACHEBUST=$(date +%s)
-docker push h2oserver/quicly-interop-runner:latest
+TAG=${TAG:=latest}
+docker build -t h2oserver/quicly-interop-runner:$TAG -f $DIRNAME/Dockerfile . --build-arg CACHEBUST=$(date +%s)
+docker push h2oserver/quicly-interop-runner:$TAG
