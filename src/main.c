e4a89d49ef42a6f4b8988b98c73fe2ee11b83dad
Goro Fuji <goro@fastly.com>
2021-03-25 04:37:56 +0000

    revert unecessary changes by clang-format

--- src/main.c
+++ src/main.c
@@ -1657,8 +1657,8 @@ static int on_config_listen(h2o_configurator_command_t *cmd, h2o_configurator_co
                     yoml_t **retry_node, **sndbuf, **rcvbuf, **amp_limit, **qpack_encoder_table_capacity, **max_streams_bidi;
                     if (h2o_configurator_parse_mapping(
                             cmd, *quic_node, NULL,
-                            "retry:s,sndbuf:s,rcvbuf:s,amp-limit:s,qpack-encoder-table-capacity:s,max-streams-bidi:s", &retry_node,
-                            &sndbuf, &rcvbuf, &amp_limit, &qpack_encoder_table_capacity, &max_streams_bidi) != 0)
+                            "retry:s,sndbuf:s,rcvbuf:s,amp-limit:s,qpack-encoder-table-capacity:s,max-streams-bidi:s",
+                            &retry_node, &sndbuf, &rcvbuf, &amp_limit, &qpack_encoder_table_capacity, &max_streams_bidi) != 0)
                         return -1;
                     if (retry_node != NULL) {
                         ssize_t on = h2o_configurator_get_one_of(cmd, *retry_node, "OFF,ON");
