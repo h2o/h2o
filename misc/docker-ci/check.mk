41af6abbed34760ae82e357d783efea2e20ee3a4
Goro Fuji <goro@fastly.com>
2021-04-07 03:48:05 +0000

    mount bpffs in CI

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -6,6 +6,7 @@ FUZZ_ASAN=ASAN_OPTIONS=detect_leaks=0
 DOCKER_RUN_OPTS=--privileged \
 	-v `pwd`:$(SRC_DIR) \
 	-v /sys/kernel/debug:/sys/kernel/debug \
+	-v /sys/fs/bpf:/sys/fs/bpf \
 	-v /lib/modules:/lib/modules:ro \
 	-v /usr/src:/usr/src:ro \
 	--add-host=127.0.0.1.xip.io:127.0.0.1 \
@@ -26,6 +27,7 @@ ossl1.1.1:
 		CMAKE_ARGS='-DOPENSSL_ROOT_DIR=/opt/openssl-1.1.1'
 
 dtrace:
+	sudo mount bpffs /sys/fs/bpf -t bpf
 	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) env DTRACE_TESTS=1 make -f $(SRC_DIR)/misc/docker-ci/check.mk _check
 
 _check:
