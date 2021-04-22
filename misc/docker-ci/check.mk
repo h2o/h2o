ac1b5196c5067af1fb8884ae6c5055ee643ce8ed
Goro Fuji <goro@fastly.com>
2021-04-22 02:23:28 +0000

    ci: set mode=700 for bpffs
    for compatibility with the latest systemd https://github.com/systemd/systemd/blob/fee6441601c979165ebcbb35472036439f8dad5f/src/shared/mount-setup.c#L111 which does mount /sys/fs/bpf

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -32,7 +32,7 @@ dtrace:
 _check:
 	uname -a
 	sudo mkdir -p /sys/fs/bpf
-	sudo mount -t bpf bpf /sys/fs/bpf
+	sudo mount -t bpf bpf /sys/fs/bpf -o mode=700
 	mkdir -p build
 	sudo mount -t tmpfs tmpfs build -o size=3G
 	sudo chown -R ci:ci build
