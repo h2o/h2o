37f7caec83eaa917a3b719544dda749cbf5489e7
Kazuho Oku <kazuhooku@gmail.com>
2021-11-23 17:25:44 +0900

    BUILD_ARGS for passing `-j6`

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -2,6 +2,7 @@ CONTAINER_NAME=h2oserver/h2o-ci:ubuntu1604
 SRC_DIR=/h2o
 CHECK_MK=$(SRC_DIR)/misc/docker-ci/check.mk
 CMAKE_ARGS=
+BUILD_ARGS=
 FUZZ_ASAN=ASAN_OPTIONS=detect_leaks=0
 DOCKER_RUN_OPTS=--privileged \
 	--ulimit memlock=-1 \
@@ -14,20 +15,27 @@ DOCKER_RUN_OPTS=--privileged \
 
 ALL:
 	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) \
-		make -f $(SRC_DIR)/misc/docker-ci/check.mk _check
+		make -f $(SRC_DIR)/misc/docker-ci/check.mk _check \
+		BUILD_ARGS='$(BUILD_ARGS)'
 
 ossl1.1.0+fuzz:
 	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) \
 		env CC=clang CXX=clang++ \
 		make -f $(SRC_DIR)/misc/docker-ci/check.mk _check \
-		CMAKE_ARGS='-DOPENSSL_ROOT_DIR=/opt/openssl-1.1.0\ -DBUILD_FUZZER=ON'
+		CMAKE_ARGS='-DOPENSSL_ROOT_DIR=/opt/openssl-1.1.0\ -DBUILD_FUZZER=ON' \
+		BUILD_ARGS='$(BUILD_ARGS)'
 
 ossl1.1.1:
-	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) make -f $(SRC_DIR)/misc/docker-ci/check.mk _check \
-		CMAKE_ARGS='-DOPENSSL_ROOT_DIR=/opt/openssl-1.1.1'
+	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) \
+		make -f $(SRC_DIR)/misc/docker-ci/check.mk _check \
+		CMAKE_ARGS='-DOPENSSL_ROOT_DIR=/opt/openssl-1.1.1' \
+		BUILD_ARGS='$(BUILD_ARGS)'
 
 dtrace:
-	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) env DTRACE_TESTS=1 make -f $(SRC_DIR)/misc/docker-ci/check.mk _check
+	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) \
+		env DTRACE_TESTS=1 \
+		make -f $(SRC_DIR)/misc/docker-ci/check.mk _check \
+		BUILD_ARGS='$(BUILD_ARGS)'
 
 _check:
 	uname -a
@@ -37,11 +45,8 @@ _check:
 	sudo mount -t tmpfs tmpfs build -o size=3G
 	sudo chown -R ci:ci build
 	sudo chmod 0755 build
-	$(MAKE) -f $(CHECK_MK) -C build _do-check CMAKE_ARGS=$(CMAKE_ARGS)
-
-_do-check:
 	cmake $(CMAKE_ARGS) -H$(SRC_DIR) -B.
-	time komake -j6 all checkdepends
+	time komake $(BUILD_ARGS) all checkdepends
 	if [ -e h2o-fuzzer-http1 ] ; then export $(FUZZ_ASAN); fi; \
 		ulimit -n 1024; \
 		make check
