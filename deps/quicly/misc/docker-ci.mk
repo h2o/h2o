d8b7a15cf94b7178b179bd273142142a483c6f16
Goro Fuji <goro@fastly.com>
2021-12-03 04:58:51 +0000

    extract https://github.com/h2o/quicly @ 240571d () at deps/quicly

--- deps/quicly/misc/docker-ci.mk
+++ deps/quicly/misc/docker-ci.mk
@@ -2,12 +2,13 @@ CONTAINER_NAME=h2oserver/h2o-ci:ubuntu2004
 SRC_DIR=/quicly
 CI_MK=$(SRC_DIR)/misc/docker-ci.mk
 CMAKE_ARGS=
+CHECK_ENVS=
 DOCKER_RUN_OPTS=--privileged \
 	-v `pwd`:$(SRC_DIR) \
 	-it
 
 ALL:
-	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) make -f $(CI_MK) _check
+	docker run $(DOCKER_RUN_OPTS) $(CONTAINER_NAME) make -f $(CI_MK) _check CMAKE_ARGS='$(CMAKE_ARGS)' CHECK_ENVS='$(CHECK_ENVS)'
 
 _check:
 	uname -a
@@ -15,9 +16,9 @@ _check:
 	sudo mount -t tmpfs tmpfs build -o size=3G
 	sudo chown -R ci:ci build
 	sudo chmod 0755 build
-	$(MAKE) -f $(CI_MK) -C build _do-check CMAKE_ARGS=$(CMAKE_ARGS)
+	$(MAKE) -f $(CI_MK) -C build _do-check CMAKE_ARGS='$(CMAKE_ARGS)' CHECK_ENVS='$(CHECK_ENVS)'
 
 _do-check:
-	cmake $(CMAKE_ARGS) -H$(SRC_DIR) -B.
-	make all
-	make check
+	cmake $(CMAKE_ARGS) "-H$(SRC_DIR)" -B.
+	$(MAKE) all VERBOSE=1
+	env $(CHECK_ENVS) $(MAKE) check
