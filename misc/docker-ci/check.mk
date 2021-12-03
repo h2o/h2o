1e5c2e94482cb142b2cd0a82918260b63fb302c1
Goro Fuji <goro@fastly.com>
2021-12-03 05:49:26 +0000

    move CMAKE_ARGS in DTrace + ASan into check.mk as the others do

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -39,9 +39,9 @@ dtrace:
 	docker run $(DOCKER_RUN_OPTS) h2oserver/h2o-ci:ubuntu2004  \
 		env DTRACE_TESTS=1 \
 		make -f $(SRC_DIR).ro/misc/docker-ci/check.mk _check \
-		CMAKE_ARGS='$(CMAKE_ARGS)' \
+		CMAKE_ARGS='-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS=-fsanitize=address -DCMAKE_CXX_FLAGS=-fsanitize=address' \
 		BUILD_ARGS='$(BUILD_ARGS)' \
-		TEST_ENV='$(TEST_ENV)'
+		TEST_ENV='ASAN_OPTIONS=detect_leaks=0:alloc_dealloc_mismatch=0 $(TEST_ENV)'
 
 _check: _mount _do_check
 
