b092dfc5a36e494a539171390e06ce234b933ab7
Kazuho Oku <kazuhooku@gmail.com>
2021-12-16 17:56:59 +0900

    clang-format

--- lib/handler/proxy.c
+++ lib/handler/proxy.c
@@ -148,13 +148,15 @@ static void on_context_init(h2o_handler_t *_self, h2o_context_t *ctx)
         .keepalive_timeout = self->config.keepalive_timeout,
         .max_buffer_size = self->config.max_buffer_size,
         .tunnel_enabled = self->config.tunnel_enabled,
-        .protocol_selector = (struct st_h2o_httpclient_protocol_selector_t){
-            .ratio = self->config.protocol_ratio,
-        },
-        .http2 = {
-            .latency_optimization = ctx->globalconf->http2.latency_optimization, /* TODO provide config knob, or disable? */
-            .max_concurrent_streams = self->config.http2.max_concurrent_streams,
-        },
+        .protocol_selector =
+            (struct st_h2o_httpclient_protocol_selector_t){
+                .ratio = self->config.protocol_ratio,
+            },
+        .http2 =
+            {
+                .latency_optimization = ctx->globalconf->http2.latency_optimization, /* TODO provide config knob, or disable? */
+                .max_concurrent_streams = self->config.http2.max_concurrent_streams,
+            },
         .http3 = self->config.protocol_ratio.http3 != 0 ? create_http3_context(ctx->loop, ctx->globalconf->http3.use_gso) : NULL,
     };
 
