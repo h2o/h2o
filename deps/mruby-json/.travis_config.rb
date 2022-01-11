4c80ee2cb2d2ad6685118e84d6865b1a66ccadb7
kody <kody.wen@hotmail.com>
2022-01-11 22:23:41 +0800

    mruby-json submodule

--- deps/mruby-json/.travis_config.rb
+++
@@ -1,8 +0,0 @@
-MRuby::Build.new do |conf|
-  toolchain :gcc
-  enable_debug
-  enable_test # IMPORTANT!
-
-  gem :core => 'mruby-print'
-  gem File.expand_path(File.dirname(__FILE__))
-end
--- deps/mruby-json/mrbgem.rake
+++
@@ -1,5 +0,0 @@
-MRuby::Gem::Specification.new('mruby-json') do |spec|
-  spec.license = 'MIT'
-  spec.authors = 'mattn'
-  spec.cc.defines << 'JSON_FIXED_NUMBER'
-end
