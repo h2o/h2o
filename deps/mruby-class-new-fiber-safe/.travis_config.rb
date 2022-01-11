3af68d047692c5a5d1f736148c2f6a417dda84cf
kody <kody.wen@hotmail.com>
2022-01-12 01:53:19 +0800

    mruby-class-new-fiber-safe submodule

--- deps/mruby-class-new-fiber-safe/.travis_config.rb
+++
@@ -1,8 +0,0 @@
-MRuby::Build.new do |conf|
-  toolchain :gcc
-  enable_debug
-  conf.enable_bintest
-  conf.enable_test
-  conf.gembox 'default'
-  conf.gem '../mruby-class-new-fiber-safe'
-end
--- deps/mruby-class-new-fiber-safe/mrblib/class-new-fiber-safe.rb
+++
@@ -1,7 +0,0 @@
-class Class
-  def new(*args, &block)
-    obj = self.allocate
-    obj.initialize(*args, &block)
-    obj
-  end
-end
