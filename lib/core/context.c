9c311fbba857f6003205f50379e4b655ec607127
Emmanuel Thompson <ethompson@fastly.com>
2022-02-08 09:50:29 -0500

    convert seconds to microseconds

--- lib/core/context.c
+++ lib/core/context.c
@@ -199,7 +199,7 @@ size_t h2o_context_close_idle_connections(h2o_context_t *ctx, size_t max_connect
     size_t closed = 0;
     H2O_CONN_LIST_FOREACH(h2o_conn_t * conn, ({&ctx->_conns.idle}), {
         struct timeval now = h2o_gettimeofday(ctx->loop);
-        if (h2o_timeval_subtract(&conn->connected_at, &now) < min_age)
+        if (h2o_timeval_subtract(&conn->connected_at, &now) < (min_age*1000*1000))
             continue;
         if (conn->callbacks->close_idle_connection(conn))
             closed++;
