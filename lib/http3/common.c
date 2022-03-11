90845e5ee1618648a62ef28eb87a5553b6df1d3d
Kazuho Oku <kazuhooku@gmail.com>
2022-03-11 15:55:25 +0900

    Use CMSG_NXTHDR to calculate the length of every CMSG by adding sentry.

--- lib/http3/common.c
+++ lib/http3/common.c
@@ -79,12 +79,10 @@ int h2o_quic_send_datagrams(h2o_quic_ctx_t *ctx, quicly_address_t *dest, quicly_
 #else
             CMSG_SPACE(1)
 #endif
-            +
 #ifdef UDP_SEGMENT
-            CMSG_SPACE(sizeof(uint16_t))
-#else
-            0
+            + CMSG_SPACE(sizeof(uint16_t))
 #endif
+            + CMSG_SPACE(1) /* this sentry makes sure that CMSG_NXTHDR succeeds; also for calculating msg_controllen at the end */
         ];
     } cmsgbuf;
     struct msghdr mess = {
@@ -93,16 +91,16 @@ int h2o_quic_send_datagrams(h2o_quic_ctx_t *ctx, quicly_address_t *dest, quicly_
         .msg_control = cmsgbuf.buf,
         .msg_controllen = sizeof(cmsgbuf.buf),
     };
-    struct cmsghdr *cmsg = NULL;
+    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&mess);
     int ret;
 
 #define PUSH_CMSG(level, type, value)                                                                                              \
     do {                                                                                                                           \
-        cmsg = cmsg == NULL ? CMSG_FIRSTHDR(&mess) : CMSG_NXTHDR(&mess, cmsg);                                                     \
         cmsg->cmsg_level = (level);                                                                                                \
         cmsg->cmsg_type = (type);                                                                                                  \
         cmsg->cmsg_len = CMSG_LEN(sizeof(value));                                                                                  \
         memcpy(CMSG_DATA(cmsg), &value, sizeof(value));                                                                            \
+        cmsg = CMSG_NXTHDR(&mess, cmsg);                                                                                           \
     } while (0)
 
     /* first CMSG is the source address */
@@ -154,12 +152,8 @@ int h2o_quic_send_datagrams(h2o_quic_ctx_t *ctx, quicly_address_t *dest, quicly_
 #endif
 
     /* commit CMSG length */
-    if (cmsg != NULL) {
-        mess.msg_controllen = (socklen_t)((char *)cmsg - (char *)cmsgbuf.buf) + cmsg->cmsg_len;
-    } else {
+    if ((mess.msg_controllen = (socklen_t)((char *)cmsg - (char *)cmsgbuf.buf)) == 0)
         mess.msg_control = NULL;
-        mess.msg_controllen = 0;
-    }
 
     /* send datagrams */
     if (using_gso) {
