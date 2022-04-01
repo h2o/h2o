0e1a110b812a6f9d935fdd3c9e5f8ed8fc05acdb
Kazuho Oku <kazuhooku@gmail.com>
2022-04-01 12:40:17 +0900

    report feature

--- src/main.c
+++ src/main.c
@@ -3646,6 +3646,9 @@ int main(int argc, char **argv)
 #if H2O_USE_DTRACE
                 printf("dtrace: YES\n");
 #endif
+#if H2O_USE_FUSION
+                printf("fusion: YES\n");
+#endif
                 exit(0);
             case 'h':
                 printf("h2o version " H2O_VERSION "\n"
