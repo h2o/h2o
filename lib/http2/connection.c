c5931cb203f373b7b999b185626fbb28f231f9b4
Kazuho Oku <kazuhooku@gmail.com>
2022-04-04 19:35:17 +0900

    simplify the condition

--- lib/http2/connection.c
+++ lib/http2/connection.c
@@ -466,7 +466,7 @@ static void stream_send_error(h2o_http2_conn_t *conn, uint32_t stream_id, int er
 static void request_gathered_write(h2o_http2_conn_t *conn)
 {
     assert(conn->state < H2O_HTTP2_CONN_STATE_IS_CLOSING);
-    if (!h2o_socket_is_writing(conn->sock) && conn->read_file.stream == NULL && !h2o_timer_is_linked(&conn->_write.timeout_entry))
+    if (conn->_write.buf_in_flight == NULL && !h2o_timer_is_linked(&conn->_write.timeout_entry))
         h2o_timer_link(conn->super.ctx->loop, 0, &conn->_write.timeout_entry);
 }
 
