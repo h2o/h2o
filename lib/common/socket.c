93b781b0addf150241b7bb09907b8d5fcccc23f8
David Carlier <devnexen@gmail.com>
2022-05-15 14:17:22 +0100

    obtain_tcp_info enabling on netbsd

--- lib/common/socket.c
+++ lib/common/socket.c
@@ -657,10 +657,19 @@ static int obtain_tcp_info(int fd, uint32_t *rtt, uint32_t *mss, uint32_t *cwnd_
     CALC_CWND_PAIR_FROM_BYTE_UNITS(tcpi.tcpi_snd_cwnd, tcpi.tcpi_snd_sbbytes);
     return 0;
 
+#elif defined(__NetBSD__) && defined(TCP_INFO)
+
+    struct tcp_info tcpi;
+    socklen_t tcpisz = sizeof(tcpi);
+    int bytes_inflight;
+    if (getsockopt(fd, IPPROTO_TCP, TCP_INFO, &tcpi, &tcpisz) != 0 || ioctl(fd, FIONWRITE, &bytes_inflight) == -1)
+        return -1;
+    *rtt = tcpi.tcpi_rtt;
+    *mss = tcpi.tcpi_snd_mss;
+    CALC_CWND_PAIR_FROM_BYTE_UNITS(tcpi.tcpi_snd_cwnd, bytes_inflight);
+    return 0;
+
 #else
-    /* TODO add support for NetBSD; note that the OS returns the number of packets for tcpi_snd_cwnd; see
-     * http://twitter.com/n_soda/status/740719125878575105
-     */
     return -1;
 #endif
 
