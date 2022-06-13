47402b919cd3dc09031413d04cb0c8eb200095c9
Emmanuel Thompson <eet6646@gmail.com>
2022-06-13 10:51:35 -0400

    reset connection to idle after processing data

--- lib/http1.c
+++ lib/http1.c
@@ -155,7 +155,7 @@ static void cleanup_connection(struct st_h2o_http1_conn_t *conn)
     }
 
     if (conn->sock->input->size == 0)
-        h2o_conn_set_state(&conn->super, H2O_CONN_STATE_SHUTDOWN);
+        h2o_conn_set_state(&conn->super, H2O_CONN_STATE_IDLE);
 
     assert(conn->req.proceed_req == NULL);
     assert(conn->_req_entity_reader == NULL);
