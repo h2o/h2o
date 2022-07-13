a0e269c2333b67796c7f10293fc5aeb20e6c0d3e
Hajime Fujita <crisp.fujita@nifty.com>
2022-07-13 08:41:29 +0000

    Use dummy variables for http3 acceptor errors
    Creating an invalid pointer from a literal value is undefined and should be avoided where possible.
    This patch introduces special dummy variables for representing http3 error values, and use their addresses as error codes.

--- lib/http3/common.c
+++ lib/http3/common.c
@@ -36,6 +36,9 @@
 #include "h2o/multithread.h"
 #include "../probes_.h"
 
+h2o_quic_conn_t h2o_quic_accept_conn_decryption_failed;
+h2o_http3_conn_t h2o_quic_accept_conn_closed;
+
 struct st_h2o_http3_ingress_unistream_t {
     /**
      * back pointer
