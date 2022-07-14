8afa8aa62fa679612b3cf8907d140936b82995c7
Kazuho Oku <kazuhooku@gmail.com>
2022-07-14 18:29:32 +0900

    add ubuntu 20.04 / openssl 3.0 to the matrix

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -35,8 +35,15 @@ ossl1.1.1:
 		BUILD_ARGS='$(BUILD_ARGS)' \
 		TEST_ENV='$(TEST_ENV)'
 
+ossl3.0:
+	docker run $(DOCKER_RUN_OPTS) h2oserver/h2o-ci:ubuntu2204 \
+		env DTRACE_TESTS=1 \
+		make -f $(SRC_DIR).ro/misc/docker-ci/check.mk _check \
+		BUILD_ARGS='$(BUILD_ARGS)' \
+		TEST_ENV='$(TEST_ENV)'
+
 dtrace+asan:
-	docker run $(DOCKER_RUN_OPTS) h2oserver/h2o-ci:ubuntu2004  \
+	docker run $(DOCKER_RUN_OPTS) h2oserver/h2o-ci:ubuntu2004 \
 		env DTRACE_TESTS=1 \
 		make -f $(SRC_DIR).ro/misc/docker-ci/check.mk _check \
 		CMAKE_ARGS='-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS=-fsanitize=address -DCMAKE_CXX_FLAGS=-fsanitize=address' \
