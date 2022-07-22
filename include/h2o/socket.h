651d9e74b94cd8d77f7de25340b2fd3beece45d3
Baodong Chen <chenbaodong@mxnavi.com>
2022-07-23 07:27:02 +0800

    'H2O_USE_SELECT'-->'H2O_USE_POLL'

--- include/h2o/socket.h
+++ include/h2o/socket.h
@@ -35,7 +35,7 @@ extern "C" {
 #include "h2o/string_.h"
 
 #ifndef H2O_USE_LIBUV
-#if H2O_USE_SELECT || H2O_USE_EPOLL || H2O_USE_KQUEUE
+#if H2O_USE_POLL || H2O_USE_EPOLL || H2O_USE_KQUEUE
 #define H2O_USE_LIBUV 0
 #else
 #define H2O_USE_LIBUV 1
