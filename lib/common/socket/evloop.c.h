07f7f8e32edd21df6f2e9e894d8c631f28da0774
Emmanuel Thompson <eet6646@gmail.com>
2022-10-05 12:19:10 -0400

    check closed

--- lib/common/socket/evloop.c.h
+++ lib/common/socket/evloop.c.h
@@ -851,7 +851,7 @@ void h2o_socket_notify_write(h2o_socket_t *_sock, h2o_socket_cb cb)
 
 static void run_socket(struct st_h2o_evloop_socket_t *sock)
 {
-    if ((sock->_flags & H2O_SOCKET_FLAG_IS_DISPOSED) != 0) {
+    if ((sock->_flags & H2O_SOCKET_FLAG_IS_CLOSED) != 0 || (sock->_flags & H2O_SOCKET_FLAG_IS_DISPOSED) != 0) {
         /* is freed in updatestates phase */
         return;
     }
