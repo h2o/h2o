4dfea8b6b242420e586032bb68fceec6e1ed30a2
Kazuho Oku <kazuhooku@gmail.com>
2022-12-15 10:52:31 +0900

    extract https://github.com/h2o/neverbleed @ 4a18917 () at deps/neverbleed

--- deps/neverbleed/neverbleed.c
+++ deps/neverbleed/neverbleed.c
@@ -932,13 +932,16 @@ static int digestsign_stub(neverbleed_iobuf_t *buf)
         if (EVP_PKEY_CTX_set_rsa_mgf1_md(pkey_ctx, md) != 1)
             goto Softfail;
     }
-    if (EVP_DigestSign(mdctx, NULL, &digestlen, signdata, signlen) != 1)
+    /* ED25519 keys can never be loaded, so use the Update -> Final call chain without worrying about backward compatibility */
+    if (EVP_DigestSignUpdate(mdctx, signdata, signlen) != 1)
+        goto Softfail;
+    if (EVP_DigestSignFinal(mdctx, NULL, &digestlen) != 1)
         goto Softfail;
     if (sizeof(digestbuf) < digestlen) {
         warnf("%s: digest unexpectedly long as %zu bytes", __FUNCTION__, digestlen);
         goto Softfail;
     }
-    if (EVP_DigestSign(mdctx, digestbuf, &digestlen, signdata, signlen) != 1)
+    if (EVP_DigestSignFinal(mdctx, digestbuf, &digestlen) != 1)
         goto Softfail;
 
 Respond: /* build response */
@@ -962,9 +965,11 @@ void neverbleed_start_digestsign(neverbleed_iobuf_t *buf, EVP_PKEY *pkey, const 
 
     /* obtain reference */
     switch (EVP_PKEY_base_id(pkey)) {
-    case EVP_PKEY_RSA:
-        get_privsep_data(EVP_PKEY_get0_RSA(pkey), &exdata, &thdata);
-        break;
+    case EVP_PKEY_RSA: {
+        RSA *rsa = EVP_PKEY_get1_RSA(pkey); /* get0 is available not available in OpenSSL 1.0.2 */
+        get_privsep_data(rsa, &exdata, &thdata);
+        RSA_free(rsa);
+    } break;
 #ifdef NEVERBLEED_ECDSA
     case EVP_PKEY_EC:
         ecdsa_get_privsep_data(EVP_PKEY_get0_EC_KEY(pkey), &exdata, &thdata);
