8c57a415015eed53fdad072398a798b19f7cf254
Kazuho Oku <kazuhooku@gmail.com>
2024-03-10 14:18:20 +0900

    follow API change

--- src/main.c
+++ src/main.c
@@ -2831,16 +2831,15 @@ static int on_config_listen_element(h2o_configurator_command_t *cmd, h2o_configu
                 if (quic_node != NULL) {
                     yoml_t **retry_node, **sndbuf, **rcvbuf, **amp_limit, **qpack_encoder_table_capacity, **max_streams_bidi,
                         **max_udp_payload_size, **handshake_timeout_rtt_multiplier, **max_initial_handshake_packets, **ecn,
-                        **pacing, **default_jumpstart_cwnd_bytes, **max_jumpstart_cwnd_bytes;
+                        **pacing, **jumpstart_default, **jumpstart_max;
                     if (h2o_configurator_parse_mapping(
                             cmd, *quic_node, NULL,
                             "retry:s,sndbuf:s,rcvbuf:s,amp-limit:s,qpack-encoder-table-capacity:s,max-"
                             "streams-bidi:s,max-udp-payload-size:s,handshake-timeout-rtt-multiplier:s,"
-                            "max-initial-handshake-packets:s,ecn:s,pacing:s,default-jumpstart-cwnd-bytes:s,"
-                            "max-jumpstart-cwnd-bytes:s",
+                            "max-initial-handshake-packets:s,ecn:s,pacing:s,jumpstart-default:s,jumpstart-max:s",
                             &retry_node, &sndbuf, &rcvbuf, &amp_limit, &qpack_encoder_table_capacity, &max_streams_bidi,
                             &max_udp_payload_size, &handshake_timeout_rtt_multiplier, &max_initial_handshake_packets, &ecn, &pacing,
-                            &default_jumpstart_cwnd_bytes, &max_jumpstart_cwnd_bytes) != 0)
+                            &jumpstart_default, &jumpstart_max) != 0)
                         return -1;
                     if (retry_node != NULL) {
                         ssize_t on = h2o_configurator_get_one_of(cmd, *retry_node, "OFF,ON");
@@ -2905,14 +2904,14 @@ static int on_config_listen_element(h2o_configurator_command_t *cmd, h2o_configu
                             return -1;
                         listener->quic.ctx->use_pacing = (unsigned)on;
                     }
-                    if (default_jumpstart_cwnd_bytes != NULL) {
-                        if (h2o_configurator_scanf(cmd, *default_jumpstart_cwnd_bytes, "%" PRIu32,
-                                                   &listener->quic.ctx->default_jumpstart_cwnd_bytes) != 0)
+                    if (jumpstart_default != NULL) {
+                        if (h2o_configurator_scanf(cmd, *jumpstart_default, "%" PRIu32,
+                                                   &listener->quic.ctx->default_jumpstart_cwnd_packets) != 0)
                             return -1;
                     }
-                    if (max_jumpstart_cwnd_bytes != NULL) {
-                        if (h2o_configurator_scanf(cmd, *max_jumpstart_cwnd_bytes, "%" PRIu32,
-                                                   &listener->quic.ctx->max_jumpstart_cwnd_bytes) != 0)
+                    if (jumpstart_max != NULL) {
+                        if (h2o_configurator_scanf(cmd, *jumpstart_max, "%" PRIu32,
+                                                   &listener->quic.ctx->max_jumpstart_cwnd_packets) != 0)
                             return -1;
                     }
                 }
