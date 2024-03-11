f9e2a5a9d5c69308aec08c6d59d001acdc883d9e
Kazuho Oku <kazuhooku@gmail.com>
2024-03-11 19:24:43 +0900

    extract https://github.com/h2o/picotest @ aa4cf1d () at deps/picotest

--- deps/picotest/picotest.h
+++ deps/picotest/picotest.h
@@ -26,11 +26,32 @@
 extern "C" {
 #endif
 
-void note(const char *fmt, ...)  __attribute__((format (printf, 1, 2)));
-void _ok(int cond, const char *fmt, ...) __attribute__((format (printf, 2, 3)));
+#ifdef _MSC_VER
+#ifndef __attribute__
+#define __attribute__(X)
+#endif
+#endif
+
+extern int test_index[32];
+
+void note(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+void _ok(int cond, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
 #define ok(cond) _ok(cond, "%s %d", __FILE__, __LINE__)
 int done_testing(void);
-void subtest(const char *name, void (*cb)(void));
+#define subtest(name, cb, ...)                                                                                                     \
+    do {                                                                                                                           \
+        const char *_name = (name);                                                                                                \
+        enter_subtest(_name);                                                                                                      \
+        (cb)(__VA_ARGS__);                                                                                                         \
+        exit_subtest(_name);                                                                                                       \
+    } while (0)
+void enter_subtest(const char *name);
+void exit_subtest(const char *name);
+/**
+ * Returns if the test is at the expected position.  For example, `test_is_at(3, 2, 0)` returns true if it is running the 3rd test at
+ * the top level, which is a subtest in which the next test to be run is the second one.  The last `0` is the terminator.
+ */
+int test_is_at(int index, ...);
 
 #ifdef __cplusplus
 }
