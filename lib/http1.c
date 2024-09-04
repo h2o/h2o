65724f0b76af2e657bef06c4469f536323e7e5bd
Kazuho Oku <kazuhooku@gmail.com>
2024-09-04 22:37:07 +0900

    do not early return, so that persistency is disabled after receiving SIGTERM

--- lib/http1.c
+++ lib/http1.c
@@ -516,11 +516,11 @@ static const char *fixup_request(struct st_h2o_http1_conn_t *conn, struct phr_he
                     if (upgrade_is_h2(upgrade) && conn->sock->ssl == NULL) {
                         if (conn->super.ctx->globalconf->http1.upgrade_to_http2)
                             return fixup_request_is_h2_upgrade;
-                        return NULL;
+                    } else {
+                        conn->req.upgrade = upgrade;
+                        conn->req.is_tunnel_req = 1;
+                        conn->req.http1_is_persistent = 0;
                     }
-                    conn->req.upgrade = upgrade;
-                    conn->req.is_tunnel_req = 1;
-                    conn->req.http1_is_persistent = 0;
                 }
             } else if (conn->req.version >= 0x101) {
                 /* defaults to keep-alive if >= HTTP/1.1 */
