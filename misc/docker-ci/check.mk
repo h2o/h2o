64392649fcbd37a996ba1c6a0a9395e072a371ed
Kazuho Oku <kazuhooku@gmail.com>
2025-04-29 10:44:20 +0900

    try adding `%m` to avoid conflicting writes from different modules within the same process

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -70,7 +70,7 @@ coverage:
 		make -f $(SRC_DIR)/misc/docker-ci/check.mk _check _coverage_report \
 		CMAKE_ARGS='-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS="-fprofile-instr-generate -fcoverage-mapping -mllvm -runtime-counter-relocation" -DCMAKE_CXX_FLAGS= -DCMAKE_BUILD_TYPE=Debug -DWITH_H2OLOG=OFF' \
 		BUILD_ARGS='$(BUILD_ARGS)' \
-		TEST_ENV='SKIP_PROG_EXISTS=1 LLVM_PROFILE_FILE=/home/ci/profraw/%c%p.profraw $(TEST_ENV)' \
+		TEST_ENV='SKIP_PROG_EXISTS=1 LLVM_PROFILE_FILE=/home/ci/profraw/%c%p-%m.profraw $(TEST_ENV)' \
 		TMP_SIZE='$(TMP_SIZE)'
 
 _coverage_report:
