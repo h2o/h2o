64616c3356b0d9affa9f1df68594501b8bb8238b
Joe Calderon <jcalderon@fastly.com>
2025-08-29 08:01:40 -0700

    include h2o.h in include/h2o/pipe_reader.h

--- include/h2o/pipe_reader.h
+++ include/h2o/pipe_reader.h
@@ -1,12 +1,7 @@
 #ifndef __H2O__PIPE_UTILS__H__
 #define __H2O__PIPE_UTILS__H__
 
-#include <stddef.h>
-#include <stdbool.h>
-
-enum h2o_send_state;
-struct st_h2o_req_t;
-struct st_h2o_context_t;
+#include "h2o.h"
 
 typedef struct st_h2o_pipe_reader_t {
     int fds[2]; /* fd[0] set to -1 unless used */
@@ -15,15 +10,15 @@ typedef struct st_h2o_pipe_reader_t {
     size_t body_bytes_read, body_bytes_sent;
 } h2o_pipe_reader_t;
 
-void h2o_pipe_reader_send(struct st_h2o_req_t *req, h2o_pipe_reader_t *pipe_reader, enum h2o_send_state send_state);
+void h2o_pipe_reader_send(h2o_req_t *req, h2o_pipe_reader_t *pipe_reader, h2o_send_state_t send_state);
 
 void h2o_pipe_reader_init(h2o_pipe_reader_t *pipe_reader);
 
-int h2o_pipe_reader_new(struct st_h2o_context_t *ctx, h2o_pipe_reader_t *pipe_reader);
+int h2o_pipe_reader_new(h2o_context_t *ctx, h2o_pipe_reader_t *pipe_reader);
 
-int h2o_pipe_reader_start(struct st_h2o_context_t *ctx, h2o_pipe_reader_t *pipe_reader);
+int h2o_pipe_reader_start(h2o_context_t *ctx, h2o_pipe_reader_t *pipe_reader);
 
-void h2o_pipe_reader_dispose(struct st_h2o_context_t *ctx, h2o_pipe_reader_t *pipe_reader);
+void h2o_pipe_reader_dispose(h2o_context_t *ctx, h2o_pipe_reader_t *pipe_reader);
 
 int h2o_pipe_reader_is_empty(h2o_pipe_reader_t *pipe_reader);
 
