c4c328a6ef1329be0d28cef3427c74f4a99e4ca2
Kazuho Oku <kazuhooku@gmail.com>
2025-09-04 14:14:47 +0900

    align the file prefix / suffix with others

--- include/h2o/pipe_sender.h
+++ include/h2o/pipe_sender.h
@@ -1,5 +1,26 @@
-#ifndef __H2O__PIPE_UTILS__H__
-#define __H2O__PIPE_UTILS__H__
+/*
+ * Copyright (c) 2022-2025 Fastly, Kazuho Oku
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#ifndef h2o__pipe_sender_h
+#define h2o__pipe_sender_h
 
 #include "h2o.h"
 
@@ -74,4 +95,4 @@ inline void h2o_pipe_sender_update(h2o_pipe_sender_t *sender, size_t read_bytes)
     sender->bytes_read = read_bytes;
 }
 
-#endif /* __H2O__PIPE_UTILS__H__ */
+#endif
