53f5f35359e9150440bb9517b8eab2d3e7bed968
Kazuho Oku <kazuhooku@gmail.com>
2025-09-12 19:08:02 +0900

    on the example client, simply use openssl-backed quiclb

--- src/httpclient.c
+++ src/httpclient.c
@@ -776,7 +776,7 @@ int main(int argc, char **argv)
         uint8_t random_key[PTLS_SHA256_DIGEST_SIZE];
         h3ctx.tls.random_bytes(random_key, sizeof(random_key));
         h3ctx.quic.cid_encryptor = quicly_new_default_cid_encryptor(
-            &ptls_openssl_bfecb, &ptls_openssl_aes128ecb, &ptls_openssl_sha256, ptls_iovec_init(random_key, sizeof(random_key)));
+            &ptls_openssl_quiclb, &ptls_openssl_aes128ecb, &ptls_openssl_sha256, ptls_iovec_init(random_key, sizeof(random_key)));
         assert(h3ctx.quic.cid_encryptor != NULL);
         ptls_clear_memory(random_key, sizeof(random_key));
     }
