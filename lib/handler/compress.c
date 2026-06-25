3036bc01de06342d21e82c96b01ca2a0c513c9cb
Kazuho Oku <kazuhooku@gmail.com>
2026-06-25 16:48:13 +0900

    remove redundant if, use the same style as the code above

--- lib/handler/compress.c
+++ lib/handler/compress.c
@@ -125,13 +125,12 @@ static void on_setup_ostream(h2o_filter_t *_self, h2o_req_t *req, h2o_ostream_t 
     } else
 #endif
 #if H2O_USE_ZSTD
-    if (self->args.zstd.quality != -1 && (compressible_types & H2O_COMPRESSIBLE_ZSTD) != 0) {
-        compressor = h2o_compress_zstd_open(&req->pool, self->args.zstd.quality, req->res.content_length,
-                                            req->preferred_chunk_size);
-    } else if (self->args.gzip.quality != -1 && (compressible_types & H2O_COMPRESSIBLE_GZIP) != 0) {
-#else
-    if (self->args.gzip.quality != -1 && (compressible_types & H2O_COMPRESSIBLE_GZIP) != 0) {
+        if (self->args.zstd.quality != -1 && (compressible_types & H2O_COMPRESSIBLE_ZSTD) != 0) {
+        compressor =
+            h2o_compress_zstd_open(&req->pool, self->args.zstd.quality, req->res.content_length, req->preferred_chunk_size);
+    } else
 #endif
+        if (self->args.gzip.quality != -1 && (compressible_types & H2O_COMPRESSIBLE_GZIP) != 0) {
         compressor = h2o_compress_gzip_open(&req->pool, self->args.gzip.quality);
     } else {
         /* let proxies know that we looked at accept-encoding when deciding not to compress */
